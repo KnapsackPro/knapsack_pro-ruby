a66f318e97e2323e15ca956956861adac29fcab9
ArturT <arturtrzop@gmail.com>
2015-07-24 20:41:54 +0200

    Use KNAPSACK_PRO_RECORDING_ENABLED flag to enable recording for test file execution and pushing data to API

--- lib/knapsack_pro/runners/rspec_runner.rb
+++ lib/knapsack_pro/runners/rspec_runner.rb
@@ -5,7 +5,7 @@ module KnapsackPro
         allocator_builder = KnapsackPro::AllocatorBuilder.new(KnapsackPro::Adapters::RspecAdapter)
         allocator = allocator_builder.allocator
 
-        cmd = %Q[bundle exec rspec #{args} --default-path #{allocator_builder.test_dir} -- #{allocator.stringify_node_tests}]
+        cmd = %Q[KNAPSACK_PRO_RECORDING_ENABLED=true bundle exec rspec #{args} --default-path #{allocator_builder.test_dir} -- #{allocator.stringify_node_tests}]
 
         system(cmd)
         exit($?.exitstatus)
