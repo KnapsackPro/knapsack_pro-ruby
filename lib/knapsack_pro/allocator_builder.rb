25a9c33466066058aa276733fa5a983f92b3a1fc
ArturT <arturtrzop@gmail.com>
2015-07-24 20:40:29 +0200

    refactor allocator builder

--- lib/knapsack_pro/allocator_builder.rb
+++ lib/knapsack_pro/allocator_builder.rb
@@ -7,8 +7,8 @@ module KnapsackPro
     def allocator
       KnapsackPro::Allocator.new({
         test_file_pattern: test_file_pattern,
-        ci_node_total: KnapsackPro::Config::Env.ci_node_total,
-        ci_node_index: KnapsackPro::Config::Env.ci_node_index,
+        ci_node_total: env.ci_node_total,
+        ci_node_index: env.ci_node_index,
       })
     end
 
@@ -20,8 +20,12 @@ module KnapsackPro
 
     attr_reader :adapter_class
 
+    def env
+      KnapsackPro::Config::Env
+    end
+
     def test_file_pattern
-      KnapsackPro::Config::Env.test_file_pattern || adapter_class::TEST_DIR_PATTERN
+      env.test_file_pattern || adapter_class::TEST_DIR_PATTERN
     end
   end
 end
