367f94865106d6c54c4de29029cfa766841cdb96
ArturT <arturtrzop@gmail.com>
2015-07-26 15:30:37 +0200

    Add RepositoryAdapterInitiator with spec

---
+++ lib/knapsack_pro/repository_adapter_initiator.rb
@@ -0,0 +1,12 @@
+module KnapsackPro
+  class RepositoryAdapterInitiator
+    def self.call
+      case KnapsackPro::Config::Env.repository_adapter
+      when 'git'
+        KnapsackPro::RepositoryAdapters::GitAdapter.new
+      else
+        KnapsackPro::RepositoryAdapters::EnvAdapter.new
+      end
+    end
+  end
+end
