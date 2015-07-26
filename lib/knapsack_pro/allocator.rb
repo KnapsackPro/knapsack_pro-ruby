c55d7d54eee50475e9a6e3d937be16a27e5d80eb
ArturT <arturtrzop@gmail.com>
2015-07-26 15:45:01 +0200

    Draft for allocator and rspec runner

--- lib/knapsack_pro/allocator.rb
+++ lib/knapsack_pro/allocator.rb
@@ -1,29 +1,51 @@
 module KnapsackPro
   class Allocator
-    attr_reader :test_dir
-
-    def initialize(test_file_pattern:,
-                   test_dir:,
+    def initialize(test_files:,
                    ci_node_total:,
-                   ci_node_index:)
-      @test_file_pattern = test_file_pattern
-      @test_dir = test_dir
+                   ci_node_index:,
+                   repository_adapter:)
+      @test_files = test_files
       @ci_node_total = ci_node_total
       @ci_node_index = ci_node_index
+      @repository_adapter = repository_adapter
     end
 
-    def node_tests
-      # TODO list of test files from API
+    def call
+      action = KnapsackPro::Client::API::V1::BuildDistributions.subset(
+        commit_hash: repository_adapter.commit_hash,
+        branch: repository_adapter.branch,
+        node_total: ci_node_total,
+        node_index: ci_node_index,
+        test_files: test_files,
+      )
+      connection = KnapsackPro::Client::Connection.new(action)
+      @response = connection.call
+      if connection.success?
+        raise response if connection.errors?
+        @node_test_files = parse_test_files
+      else
+        # TODO dumb test suite split when API doesn't respond
+      end
     end
 
-    def stringify_node_tests
-      node_tests.join(' ')
+    def node_test_files
+      @node_test_files
+    end
+
+    def stringify_node_test_files
+      node_test_files.join(' ')
     end
 
     private
 
-    attr_reader :test_file_pattern,
+    attr_reader :test_files,
       :ci_node_total,
-      :ci_node_index
+      :ci_node_index,
+      :repository_adapter,
+      :response
+
+    def parse_test_files
+      response['test_file'].map { |t| t['path'] }
+    end
   end
 end
--- lib/knapsack_pro/runners/rspec_runner.rb
+++ lib/knapsack_pro/runners/rspec_runner.rb
@@ -2,9 +2,15 @@ module KnapsackPro
   module Runners
     class RSpecRunner
       def self.run(args)
-        allocator = KnapsackPro::AllocatorBuilder.call(KnapsackPro::Adapters::RSpecAdapter)
+        adapter_class = KnapsackPro::Adapters::RSpecAdapter
+        allocator_builder = KnapsackPro::AllocatorBuilder.new(adapter_class)
+        test_dir = allocator_builder.test_dir
 
-        cmd = %Q[KNAPSACK_PRO_RECORDING_ENABLED=true bundle exec rspec #{args} --default-path #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
+        allocator = allocator_builder.allocator
+        allocator.call
+        stringify_node_test_files = allocator.stringify_node_test_files
+
+        cmd = %Q[KNAPSACK_PRO_RECORDING_ENABLED=true bundle exec rspec #{args} --default-path #{test_dir} -- #{stringify_node_test_files}]
 
         system(cmd)
         exit($?.exitstatus)
