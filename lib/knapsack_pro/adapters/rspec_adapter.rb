245d9c1fdbd929bd507b417dd5b475701c302ac7
ArturT <arturtrzop@gmail.com>
2015-07-31 15:47:48 +0200

    Add RSpecAdapter with spec. TODO: add save report implementation.

--- lib/knapsack_pro/adapters/rspec_adapter.rb
+++ lib/knapsack_pro/adapters/rspec_adapter.rb
@@ -1,6 +1,48 @@
 module KnapsackPro
   module Adapters
     class RSpecAdapter
+      TEST_DIR_PATTERN = 'spec/**/*_spec.rb'
+
+      def self.test_path(example_group)
+        unless example_group[:turnip]
+          until example_group[:parent_example_group].nil?
+            example_group = example_group[:parent_example_group]
+          end
+        end
+
+        example_group[:file_path]
+      end
+
+      def bind_time_tracker
+        ::RSpec.configure do |config|
+          config.before(:each) do
+            current_example_group =
+              if ::RSpec.respond_to?(:current_example)
+                ::RSpec.current_example.metadata[:example_group]
+              else
+                example.metadata
+              end
+            KnapsackPro.tracker.current_test_path = KnapsackPro::Adapters::RSpecAdapter.test_path(current_example_group)
+            KnapsackPro.tracker.start_timer
+          end
+
+          config.after(:each) do
+            KnapsackPro.tracker.stop_timer
+          end
+
+          config.after(:suite) do
+            KnapsackPro.logger.info(KnapsackPro::Presenter.global_time)
+          end
+        end
+      end
+
+      def bind_save_report
+        ::RSpec.configure do |config|
+          config.after(:suite) do
+            # TODO push results to API
+          end
+        end
+      end
     end
   end
 end
--- spec/knapsack_pro/adapters/rspec_adapter_spec.rb
+++ spec/knapsack_pro/adapters/rspec_adapter_spec.rb
@@ -1,3 +1,97 @@
 describe KnapsackPro::Adapters::RSpecAdapter do
+  it do
+    expect(described_class::TEST_DIR_PATTERN).to eq 'spec/**/*_spec.rb'
+  end
 
+  context do
+    before { expect(::RSpec).to receive(:configure) }
+    it_behaves_like 'adapter'
+  end
+
+  describe 'bind methods' do
+    let(:config) { double }
+
+    describe '#bind_time_tracker' do
+      let(:logger) { instance_double(Logger) }
+      let(:tracker) { instance_double(KnapsackPro::Tracker) }
+      let(:test_path) { 'spec/a_spec.rb' }
+      let(:global_time) { 'Global time: 01m 05s' }
+      let(:example_group) { double }
+      let(:current_example) do
+        OpenStruct.new(metadata: {
+          example_group: example_group
+        })
+      end
+
+      before do
+        expect(KnapsackPro).to receive(:logger).and_return(logger)
+      end
+
+      it do
+        expect(config).to receive(:before).with(:each).and_yield
+        expect(config).to receive(:after).with(:each).and_yield
+        expect(config).to receive(:after).with(:suite).and_yield
+        expect(::RSpec).to receive(:configure).and_yield(config)
+
+        expect(::RSpec).to receive(:current_example).twice.and_return(current_example)
+        expect(described_class).to receive(:test_path).with(example_group).and_return(test_path)
+
+        allow(KnapsackPro).to receive(:tracker).and_return(tracker)
+        expect(tracker).to receive(:current_test_path=).with(test_path)
+        expect(tracker).to receive(:start_timer)
+
+        expect(tracker).to receive(:stop_timer)
+
+        expect(KnapsackPro::Presenter).to receive(:global_time).and_return(global_time)
+        expect(logger).to receive(:info).with(global_time)
+
+        subject.bind_time_tracker
+      end
+    end
+
+    describe '#bind_save_report' do
+      it do
+        expect(config).to receive(:after).with(:suite).and_yield
+        expect(::RSpec).to receive(:configure).and_yield(config)
+
+        # TODO
+
+        subject.bind_save_report
+      end
+    end
+  end
+
+  describe '.test_path' do
+    let(:current_example_metadata) do
+      {
+        file_path: '1_shared_example.rb',
+        parent_example_group: {
+          file_path: '2_shared_example.rb',
+          parent_example_group: {
+            file_path: 'a_spec.rb'
+          }
+        }
+      }
+    end
+
+    subject { described_class.test_path(current_example_metadata) }
+
+    it { should eql 'a_spec.rb' }
+
+    context 'with turnip features' do
+      let(:current_example_metadata) do
+        {
+          file_path: "./spec/features/logging_in.feature",
+          turnip: true,
+          parent_example_group: {
+            file_path: "gems/turnip-1.2.4/lib/turnip/rspec.rb",
+          }
+        }
+      end
+
+      subject { described_class.test_path(current_example_metadata) }
+
+      it { should eql './spec/features/logging_in.feature' }
+    end
+  end
 end
