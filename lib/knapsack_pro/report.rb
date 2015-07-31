1abc68d80d452917ec256d0e04c3be070a531b8b
ArturT <arturtrzop@gmail.com>
2015-07-31 16:49:39 +0200

    Add report with spec

---
+++ lib/knapsack_pro/report.rb
@@ -0,0 +1,20 @@
+module KnapsackPro
+  class Report
+    def self.save
+      repository_adapter = KnapsackPro::RepositoryAdapterInitiator.call
+      action = KnapsackPro::Client::API::V1::BuildSubsets.create(
+        commit_hash: repository_adapter.commit_hash,
+        branch: repository_adapter.branch,
+        node_total: KnapsackPro::Config::Env.ci_node_total,
+        node_index: KnapsackPro::Config::Env.ci_node_index,
+        test_files: KnapsackPro.tracker.to_a,
+      )
+      connection = KnapsackPro::Client::Connection.new(action)
+      response = connection.call
+      if connection.success?
+        raise ArgumentError.new(response) if connection.errors?
+        KnapsackPro.logger.info('Knapsack Pro saved time execution report on API server.')
+      end
+    end
+  end
+end
---
+++ spec/knapsack_pro/report_spec.rb
@@ -0,0 +1,68 @@
+describe KnapsackPro::Report do
+  describe '.save' do
+    subject { described_class.save }
+
+    before do
+      commit_hash = double
+      branch = double
+      repository_adapter = instance_double(KnapsackPro::RepositoryAdapters::EnvAdapter, commit_hash: commit_hash, branch: branch)
+      expect(KnapsackPro::RepositoryAdapterInitiator).to receive(:call).and_return(repository_adapter)
+
+      node_total = double
+      node_index = double
+      expect(KnapsackPro::Config::Env).to receive(:ci_node_total).and_return(node_total)
+      expect(KnapsackPro::Config::Env).to receive(:ci_node_index).and_return(node_index)
+
+      test_files = double
+      tracker = instance_double(KnapsackPro::Tracker, to_a: test_files)
+      expect(KnapsackPro).to receive(:tracker).and_return(tracker)
+
+      action = double
+      expect(KnapsackPro::Client::API::V1::BuildSubsets).to receive(:create).with({
+        commit_hash: commit_hash,
+        branch: branch,
+        node_total: node_total,
+        node_index: node_index,
+        test_files: test_files,
+      }).and_return(action)
+
+      connection = instance_double(KnapsackPro::Client::Connection, success?: success?, errors?: errors?)
+      expect(KnapsackPro::Client::Connection).to receive(:new).with(action).and_return(connection).and_return(connection)
+
+      response = double
+      expect(connection).to receive(:call).and_return(response)
+    end
+
+    context 'when success' do
+      let(:success?) { true }
+
+      context 'when response has errors' do
+        let(:errors?) { true }
+
+        it do
+          expect {
+            subject
+          }.to raise_error(ArgumentError)
+        end
+      end
+
+      context 'when response has no errors' do
+        let(:errors?) { false }
+
+        it do
+          logger = instance_double(Logger)
+          expect(KnapsackPro).to receive(:logger).and_return(logger)
+          expect(logger).to receive(:info).with('Knapsack Pro saved time execution report on API server.')
+          subject
+        end
+      end
+    end
+
+    context 'when failure' do
+      let(:success?) { false }
+      let(:errors?) { nil }
+
+      it { subject }
+    end
+  end
+end
