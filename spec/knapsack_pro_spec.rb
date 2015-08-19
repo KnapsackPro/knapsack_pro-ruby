0fa8f3c8c6207bd5b3ac7a3d433295d6400a68c6
ArturT <arturtrzop@gmail.com>
2015-08-20 00:58:18 +0200

    Add missing spec for KnapsackPro module

--- spec/knapsack_pro_spec.rb
+++ spec/knapsack_pro_spec.rb
@@ -1,4 +1,45 @@
 describe KnapsackPro do
+  describe '.root' do
+    subject { described_class.root }
+
+    it { expect(subject).to match 'knapsack' }
+  end
+
+  describe '.logger' do
+    subject { described_class.logger }
+
+    before { described_class.logger = nil }
+    after  { described_class.logger = nil }
+
+    context 'when default logger' do
+      let(:logger) { instance_double(Logger) }
+
+      before do
+        expect(Logger).to receive(:new).and_return(logger)
+        expect(logger).to receive(:level=).with(Logger::WARN)
+      end
+
+      it { should eql logger }
+    end
+
+    context 'when custom logger' do
+      let(:logger) { double('custom logger') }
+
+      before do
+        described_class.logger = logger
+      end
+
+      it { should eql logger }
+    end
+  end
+
+  describe '.tracker' do
+    subject { described_class.tracker }
+
+    it { should be_a KnapsackPro::Tracker }
+    it { expect(subject.object_id).to eql described_class.tracker.object_id }
+  end
+
   describe '.load_tasks' do
     let(:task_loader) { instance_double(KnapsackPro::TaskLoader) }
 
