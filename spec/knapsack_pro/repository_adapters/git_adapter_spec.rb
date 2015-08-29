a369ef437b7711875536eaa0e29e54edad0fa1e4
ArturT <arturtrzop@gmail.com>
2015-08-29 16:35:09 +0200

    Fix git adapter spec

--- spec/knapsack_pro/repository_adapters/git_adapter_spec.rb
+++ spec/knapsack_pro/repository_adapters/git_adapter_spec.rb
@@ -1,4 +1,7 @@
 describe KnapsackPro::RepositoryAdapters::GitAdapter do
+  let!(:circle_sha1) { ENV['CIRCLE_SHA1'] }
+  let!(:circle_branch) { ENV['CIRCLE_BRANCH'] }
+
   before do
     stub_const('ENV', {
       'KNAPSACK_PRO_PROJECT_DIR' => KnapsackPro.root,
@@ -12,13 +15,13 @@ describe KnapsackPro::RepositoryAdapters::GitAdapter do
 
     it { should_not be_nil }
     its(:size) { should eq 40 }
-    it { should eq ENV['CIRCLE_SHA1'] } if ENV['CIRCLE_SHA1']
+    it { should eq circle_sha1 } if ENV['CIRCLE_SHA1']
   end
 
   describe '#branch' do
     subject { described_class.new.branch }
 
     it { should_not be_nil }
-    it { should eq ENV['CIRCLE_BRANCH'] } if ENV['CIRCLE_BRANCH']
+    it { should eq circle_branch } if ENV['CIRCLE_BRANCH']
   end
 end
