56ac6589e7f1119946effcfe1153107b744d5860
ArturT <arturtrzop@gmail.com>
2015-10-23 11:44:54 +0200

    Use zero? method https://github.com/ArturT/knapsack/pull/25

--- lib/knapsack_pro/runners/cucumber_runner.rb
+++ lib/knapsack_pro/runners/cucumber_runner.rb
@@ -9,7 +9,7 @@ module KnapsackPro
         cmd = %Q[KNAPSACK_PRO_RECORDING_ENABLED=true KNAPSACK_PRO_TEST_SUITE_TOKEN=#{ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN']} bundle exec cucumber #{args} -- #{runner.stringify_test_file_paths}]
 
         Kernel.system(cmd)
-        Kernel.exit($?.exitstatus) unless $?.exitstatus == 0
+        Kernel.exit($?.exitstatus) unless $?.exitstatus.zero?
       end
     end
   end
