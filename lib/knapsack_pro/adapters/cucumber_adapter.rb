85ff789b575612e5ebfc491225b5bb1953f98712
ArturT <arturtrzop@gmail.com>
2016-01-31 14:48:57 +0100

    Add support for cucumber 2

--- lib/knapsack_pro/adapters/cucumber_adapter.rb
+++ lib/knapsack_pro/adapters/cucumber_adapter.rb
@@ -3,17 +3,23 @@ module KnapsackPro
     class CucumberAdapter < BaseAdapter
       TEST_DIR_PATTERN = 'features/**{,/*/**}/*.feature'
 
-      def self.test_path(scenario_or_outline_table)
-        if scenario_or_outline_table.respond_to?(:file)
-          scenario_or_outline_table.file
+      def self.test_path(object)
+        if Cucumber::VERSION.to_i >= 2
+          test_case = object
+          test_case.location.file
         else
-          scenario_or_outline_table.scenario_outline.file
+          scenario_or_outline_table = object
+          if scenario_or_outline_table.respond_to?(:file)
+            scenario_or_outline_table.file
+          else
+            scenario_or_outline_table.scenario_outline.file
+          end
         end
       end
 
       def bind_time_tracker
-        Around do |scenario_or_outline_table, block|
-          KnapsackPro.tracker.current_test_path = KnapsackPro::Adapters::CucumberAdapter.test_path(scenario_or_outline_table)
+        Around do |object, block|
+          KnapsackPro.tracker.current_test_path = KnapsackPro::Adapters::CucumberAdapter.test_path(object)
           KnapsackPro.tracker.start_timer
           block.call
           KnapsackPro.tracker.stop_timer
--- spec/knapsack_pro/adapters/cucumber_adapter_spec.rb
+++ spec/knapsack_pro/adapters/cucumber_adapter_spec.rb
@@ -13,48 +13,91 @@ describe KnapsackPro::Adapters::CucumberAdapter do
   end
 
   describe '.test_path' do
-    subject { described_class.test_path(scenario_or_outline_table) }
+    context 'when Cucumber version 1' do
+      subject { described_class.test_path(scenario_or_outline_table) }
 
-    context 'when scenario' do
-      let(:scenario_file) { 'features/scenario.feature' }
-      let(:scenario_or_outline_table) { double(file: scenario_file) }
+      before { stub_const('Cucumber::VERSION', '1') }
 
-      it { should eql scenario_file }
-    end
+      context 'when scenario' do
+        let(:scenario_file) { 'features/scenario.feature' }
+        let(:scenario_or_outline_table) { double(file: scenario_file) }
 
-    context 'when scenario outline' do
-      let(:scenario_outline_file) { 'features/scenario_outline.feature' }
-      let(:scenario_or_outline_table) do
-        double(scenario_outline: double(file: scenario_outline_file))
+        it { should eql scenario_file }
       end
 
-      it { should eql scenario_outline_file }
+      context 'when scenario outline' do
+        let(:scenario_outline_file) { 'features/scenario_outline.feature' }
+        let(:scenario_or_outline_table) do
+          double(scenario_outline: double(file: scenario_outline_file))
+        end
+
+        it { should eql scenario_outline_file }
+      end
+    end
+
+    context 'when Cucumber version 2' do
+      let(:file) { 'features/a.feature' }
+      let(:test_case) { double(location: double(file: file)) } # Cucumber 2
+
+      subject { described_class.test_path(test_case) }
+
+      before { stub_const('Cucumber::VERSION', '2') }
+
+      it { should eql file }
     end
   end
 
   describe 'bind methods' do
     describe '#bind_time_tracker' do
       let(:file) { 'features/a.feature' }
-      let(:scenario) { double(file: file) }
       let(:block) { double }
       let(:tracker) { instance_double(KnapsackPro::Tracker) }
       let(:logger) { instance_double(Logger) }
       let(:global_time) { 'Global time: 01m 05s' }
 
-      it do
-        expect(subject).to receive(:Around).and_yield(scenario, block)
-        allow(KnapsackPro).to receive(:tracker).and_return(tracker)
-        expect(tracker).to receive(:current_test_path=).with(file)
-        expect(tracker).to receive(:start_timer)
-        expect(block).to receive(:call)
-        expect(tracker).to receive(:stop_timer)
+      context 'when Cucumber version 1' do
+        let(:scenario) { double(file: file) }
 
-        expect(::Kernel).to receive(:at_exit).and_yield
-        expect(KnapsackPro::Presenter).to receive(:global_time).and_return(global_time)
-        expect(KnapsackPro).to receive(:logger).and_return(logger)
-        expect(logger).to receive(:info).with(global_time)
+        before { stub_const('Cucumber::VERSION', '1.3.20') }
 
-        subject.bind_time_tracker
+        it do
+          expect(subject).to receive(:Around).and_yield(scenario, block)
+          allow(KnapsackPro).to receive(:tracker).and_return(tracker)
+          expect(tracker).to receive(:current_test_path=).with(file)
+          expect(tracker).to receive(:start_timer)
+          expect(block).to receive(:call)
+          expect(tracker).to receive(:stop_timer)
+
+          expect(::Kernel).to receive(:at_exit).and_yield
+          expect(KnapsackPro::Presenter).to receive(:global_time).and_return(global_time)
+          expect(KnapsackPro).to receive(:logger).and_return(logger)
+          expect(logger).to receive(:info).with(global_time)
+
+          subject.bind_time_tracker
+        end
+      end
+
+      context 'when Cucumber version 2' do
+        let(:test_case) { double(location: double(file: file)) }
+
+        # complex version name to ensure we can catch that too
+        before { stub_const('Cucumber::VERSION', '2.0.0.rc.5') }
+
+        it do
+          expect(subject).to receive(:Around).and_yield(test_case, block)
+          allow(KnapsackPro).to receive(:tracker).and_return(tracker)
+          expect(tracker).to receive(:current_test_path=).with(file)
+          expect(tracker).to receive(:start_timer)
+          expect(block).to receive(:call)
+          expect(tracker).to receive(:stop_timer)
+
+          expect(::Kernel).to receive(:at_exit).and_yield
+          expect(KnapsackPro::Presenter).to receive(:global_time).and_return(global_time)
+          expect(KnapsackPro).to receive(:logger).and_return(logger)
+          expect(logger).to receive(:info).with(global_time)
+
+          subject.bind_time_tracker
+        end
       end
     end
 
