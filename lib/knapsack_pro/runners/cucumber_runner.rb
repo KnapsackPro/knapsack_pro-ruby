7bd9fb59c32431a822648a263de11feb6b57804e
ArturT <arturtrzop@gmail.com>
2016-09-25 14:22:42 +0200

    Use cucumber rake task

--- lib/knapsack_pro/runners/cucumber_runner.rb
+++ lib/knapsack_pro/runners/cucumber_runner.rb
@@ -3,13 +3,21 @@ module KnapsackPro
     class CucumberRunner < BaseRunner
       def self.run(args)
         ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN'] = KnapsackPro::Config::Env.test_suite_token_cucumber
+        ENV['KNAPSACK_PRO_RECORDING_ENABLED'] = 'true'
 
         runner = new(KnapsackPro::Adapters::CucumberAdapter)
 
-        cmd = %Q[KNAPSACK_PRO_RECORDING_ENABLED=true KNAPSACK_PRO_TEST_SUITE_TOKEN=#{ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN']} bundle exec cucumber #{args} -- #{runner.stringify_test_file_paths}]
+        require 'cucumber/rake/task'
 
-        Kernel.system(cmd)
-        Kernel.exit($?.exitstatus) unless $?.exitstatus.zero?
+        task_name = :features
+        if Rake::Task.task_defined?(task_name)
+          Rake::Task[task_name].clear
+        end
+
+        Cucumber::Rake::Task.new(:features) do |t|
+          t.cucumber_opts = "#{args} -- #{runner.stringify_test_file_paths}"
+        end
+        Rake::Task[task_name].invoke
       end
     end
   end
