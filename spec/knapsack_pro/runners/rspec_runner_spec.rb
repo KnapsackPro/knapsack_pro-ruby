17e7323e6b27cea9fdeb98fd2f9e368f77a3259a
ArturT <arturtrzop@gmail.com>
2016-09-30 13:24:25 +0200

    Fix spec for rspec runner

--- spec/knapsack_pro/runners/rspec_runner_spec.rb
+++ spec/knapsack_pro/runners/rspec_runner_spec.rb
@@ -1,3 +1,5 @@
+require 'rspec/core/rake_task'
+
 describe KnapsackPro::Runners::RSpecRunner do
   subject { described_class.new(KnapsackPro::Adapters::RSpecAdapter) }
 
@@ -6,43 +8,58 @@ describe KnapsackPro::Runners::RSpecRunner do
   describe '.run' do
     let(:args) { '--profile --color' }
 
-    after { described_class.run(args) }
+    let(:test_suite_token_rspec) { 'fake-token' }
+    let(:test_dir) { 'fake-test-dir' }
+    let(:test_file_paths) { double(:test_file_paths) }
+    let(:runner) do
+      instance_double(described_class,
+                      test_dir: test_dir,
+                      test_file_paths: test_file_paths)
+    end
+    let(:task) { double }
+
+    subject { described_class.run(args) }
 
     before do
-      stub_const("ENV", { 'KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC' => 'rspec-token' })
+      expect(KnapsackPro::Config::Env).to receive(:test_suite_token_rspec).and_return(test_suite_token_rspec)
 
-      stringify_test_file_paths = 'spec/a_spec.rb spec/b_spec.rb'
-      runner = instance_double(described_class,
-                               test_dir: 'spec',
-                               stringify_test_file_paths: stringify_test_file_paths)
+      expect(ENV).to receive(:[]=).with('KNAPSACK_PRO_TEST_SUITE_TOKEN', test_suite_token_rspec)
+      expect(ENV).to receive(:[]=).with('KNAPSACK_PRO_RECORDING_ENABLED', 'true')
+
       expect(described_class).to receive(:new)
       .with(KnapsackPro::Adapters::RSpecAdapter).and_return(runner)
 
-      expect(Kernel).to receive(:system)
-      .with('KNAPSACK_PRO_RECORDING_ENABLED=true KNAPSACK_PRO_TEST_SUITE_TOKEN=rspec-token bundle exec rspec --profile --color --default-path spec -- spec/a_spec.rb spec/b_spec.rb')
+      expect(Rake::Task).to receive(:[]).with('knapsack_pro:rspec_run').at_least(1).and_return(task)
+
+      t = double
+      expect(RSpec::Core::RakeTask).to receive(:new).with('knapsack_pro:rspec_run').and_yield(t)
+      expect(t).to receive(:rspec_opts=).with('--profile --color --default-path fake-test-dir')
+      expect(t).to receive(:pattern=).with(test_file_paths)
     end
 
-    context 'when command exit with success code' do
-      let(:exitstatus) { 0 }
-
+    context 'when task already exists' do
       before do
-        expect($?).to receive(:exitstatus).and_return(exitstatus)
+        expect(Rake::Task).to receive(:task_defined?).with('knapsack_pro:rspec_run').and_return(true)
+        expect(task).to receive(:clear)
       end
 
       it do
-        expect(Kernel).not_to receive(:exit)
+        result = double(:result)
+        expect(task).to receive(:invoke).and_return(result)
+        expect(subject).to eq result
       end
     end
 
-    context 'when command exit without success code' do
-      let(:exitstatus) { 1 }
-
+    context "when task doesn't exist" do
       before do
-        expect($?).to receive(:exitstatus).twice.and_return(exitstatus)
+        expect(Rake::Task).to receive(:task_defined?).with('knapsack_pro:rspec_run').and_return(false)
+        expect(task).not_to receive(:clear)
       end
 
       it do
-        expect(Kernel).to receive(:exit).with(exitstatus)
+        result = double(:result)
+        expect(task).to receive(:invoke).and_return(result)
+        expect(subject).to eq result
       end
     end
   end
