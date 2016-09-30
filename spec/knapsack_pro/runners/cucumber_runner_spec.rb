27c6e281fb8f4570d19f656ee0d7e1bb357524dc
ArturT <arturtrzop@gmail.com>
2016-09-30 13:44:07 +0200

    Fix cucumber runner spec

--- spec/knapsack_pro/runners/cucumber_runner_spec.rb
+++ spec/knapsack_pro/runners/cucumber_runner_spec.rb
@@ -1,3 +1,5 @@
+require 'cucumber/rake/task'
+
 describe KnapsackPro::Runners::CucumberRunner do
   subject { described_class.new(KnapsackPro::Adapters::CucumberAdapter) }
 
@@ -6,41 +8,59 @@ describe KnapsackPro::Runners::CucumberRunner do
   describe '.run' do
     let(:args) { '--custom-arg' }
 
-    after { described_class.run(args) }
+    subject { described_class.run(args) }
+
+    let(:test_suite_token_cucumber) { 'fake-token' }
+    let(:test_dir) { 'fake-test-dir' }
+    let(:stringify_test_file_paths) { 'features/fake1.scenario features/fake2.scenario' }
+    let(:runner) do
+      instance_double(described_class,
+                      test_dir: test_dir,
+                      stringify_test_file_paths: stringify_test_file_paths)
+    end
+    let(:task) { double }
+
+    subject { described_class.run(args) }
 
     before do
-      stub_const("ENV", { 'KNAPSACK_PRO_TEST_SUITE_TOKEN_CUCUMBER' => 'cucumber-token' })
+      expect(KnapsackPro::Config::Env).to receive(:test_suite_token_cucumber).and_return(test_suite_token_cucumber)
 
-      stringify_test_file_paths = 'features/a.feature features/b.feature'
-      runner = instance_double(described_class,
-                               stringify_test_file_paths: stringify_test_file_paths)
+      expect(ENV).to receive(:[]=).with('KNAPSACK_PRO_TEST_SUITE_TOKEN', test_suite_token_cucumber)
+      expect(ENV).to receive(:[]=).with('KNAPSACK_PRO_RECORDING_ENABLED', 'true')
+
       expect(described_class).to receive(:new)
       .with(KnapsackPro::Adapters::CucumberAdapter).and_return(runner)
 
-      expect(Kernel).to receive(:system).with('KNAPSACK_PRO_RECORDING_ENABLED=true KNAPSACK_PRO_TEST_SUITE_TOKEN=cucumber-token bundle exec cucumber --custom-arg -- features/a.feature features/b.feature')
+      expect(Rake::Task).to receive(:[]).with('knapsack_pro:cucumber_run').at_least(1).and_return(task)
+
+      t = double
+      expect(Cucumber::Rake::Task).to receive(:new).with('knapsack_pro:cucumber_run').and_yield(t)
+      expect(t).to receive(:cucumber_opts=).with('--custom-arg --require fake-test-dir -- features/fake1.scenario features/fake2.scenario')
     end
 
-    context 'when command exit with success code' do
-      let(:exitstatus) { 0 }
-
+    context 'when task already exists' do
       before do
-        expect($?).to receive(:exitstatus).and_return(exitstatus)
+        expect(Rake::Task).to receive(:task_defined?).with('knapsack_pro:cucumber_run').and_return(true)
+        expect(task).to receive(:clear)
       end
 
       it do
-        expect(Kernel).not_to receive(:exit)
+        result = double(:result)
+        expect(task).to receive(:invoke).and_return(result)
+        expect(subject).to eq result
       end
     end
 
-    context 'when command exit without success code' do
-      let(:exitstatus) { 1 }
-
+    context "when task doesn't exist" do
       before do
-        expect($?).to receive(:exitstatus).twice.and_return(exitstatus)
+        expect(Rake::Task).to receive(:task_defined?).with('knapsack_pro:cucumber_run').and_return(false)
+        expect(task).not_to receive(:clear)
       end
 
       it do
-        expect(Kernel).to receive(:exit).with(exitstatus)
+        result = double(:result)
+        expect(task).to receive(:invoke).and_return(result)
+        expect(subject).to eq result
       end
     end
   end
