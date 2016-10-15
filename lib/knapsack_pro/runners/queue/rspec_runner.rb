06eaf53d1e2753458360b8c7f82992141e1c86bb
ArturT <arturtrzop@gmail.com>
2016-10-15 21:37:41 +0200

    run tests in queue

--- lib/knapsack_pro/runners/queue/rspec_runner.rb
+++ lib/knapsack_pro/runners/queue/rspec_runner.rb
@@ -2,6 +2,35 @@ module KnapsackPro
   module Runners
     module Queue
       class RSpecRunner < BaseRunner
+        class Queue
+          def initialize(test_file_paths)
+            @test_file_paths = test_file_paths
+          end
+
+          def test_file_paths
+            @test_file_paths.shift
+          end
+        end
+
+        def self.run_tests(queue, args, runner)
+          test_file_paths = queue.test_file_paths
+
+          unless test_file_paths.nil?
+            task_name = "knapsack_pro:queue:rspec_run_#{SecureRandom.uuid}"
+
+            RSpec::Core::RakeTask.new(task_name) do |t|
+              t.rspec_opts = "#{args} --default-path #{runner.test_dir}"
+              t.pattern = test_file_paths
+            end
+
+            Rake::Task[task_name].invoke
+
+            at_exit do
+              run_tests(queue, args, runner) if $!.nil?
+            end
+          end
+        end
+
         def self.run(args)
           ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN'] = KnapsackPro::Config::Env.test_suite_token_rspec
           ENV['KNAPSACK_PRO_RECORDING_ENABLED'] = 'true'
@@ -11,16 +40,8 @@ module KnapsackPro
           if runner.test_files_to_execute_exist?
             require 'rspec/core/rake_task'
 
-            task_name = 'knapsack_pro:queue:rspec_run'
-            if Rake::Task.task_defined?(task_name)
-              Rake::Task[task_name].clear
-            end
-
-            RSpec::Core::RakeTask.new(task_name) do |t|
-              t.rspec_opts = "#{args} --default-path #{runner.test_dir}"
-              t.pattern = runner.test_file_paths
-            end
-            Rake::Task[task_name].invoke
+            queue = Queue.new(runner.test_file_paths)
+            run_tests(queue, args, runner)
           end
         end
       end
