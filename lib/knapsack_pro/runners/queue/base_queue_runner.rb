6deeaf77a1475ce55ee11d1c16259e426c3fa17c
ArturT <arturtrzop@gmail.com>
2016-10-23 18:53:44 +0200

    Get test file paths from queue API

--- lib/knapsack_pro/runners/queue/base_queue_runner.rb
+++ lib/knapsack_pro/runners/queue/base_queue_runner.rb
@@ -6,6 +6,10 @@ module KnapsackPro
           raise NotImplementedError
         end
 
+        def self.run_tests(runner, can_initialize_queue, args, exitstatus)
+          raise NotImplementedError
+        end
+
         def initialize(adapter_class)
           @allocator_builder = KnapsackPro::QueueAllocatorBuilder.new(adapter_class)
           @allocator = allocator_builder.allocator
--- lib/knapsack_pro/runners/queue/rspec_runner.rb
+++ lib/knapsack_pro/runners/queue/rspec_runner.rb
@@ -2,18 +2,19 @@ module KnapsackPro
   module Runners
     module Queue
       class RSpecRunner < BaseQueueRunner
-        class Queue
-          def initialize(test_file_paths)
-            @test_file_paths = test_file_paths
-          end
+        def self.run(args)
+          require 'rspec/core/rake_task'
 
-          def test_file_paths
-            @test_file_paths.shift
-          end
+          ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN'] = KnapsackPro::Config::Env.test_suite_token_rspec
+          ENV['KNAPSACK_PRO_QUEUE_RECORDING_ENABLED'] = 'true'
+          ENV['KNAPSACK_PRO_QUEUE_ID'] = KnapsackPro::Config::EnvGenerator.set_queue_id
+
+          runner = new(KnapsackPro::Adapters::RSpecAdapter)
+          run_tests(runner, true, args, 0)
         end
 
-        def self.run_tests(queue, args, runner, exitstatus)
-          test_file_paths = queue.test_file_paths
+        def self.run_tests(runner, can_initialize_queue, args, exitstatus)
+          test_file_paths = runner.test_file_paths(can_initialize_queue: can_initialize_queue)
 
           if test_file_paths.nil?
             KnapsackPro::Report.save_node_queue_to_api
@@ -38,23 +39,8 @@ module KnapsackPro
             end
 
             at_exit do
-              run_tests(queue, args, runner, exitstatus)
+              run_tests(runner, false, args, exitstatus)
             end
-          end
-        end
-
-        def self.run(args)
-          ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN'] = KnapsackPro::Config::Env.test_suite_token_rspec
-          ENV['KNAPSACK_PRO_QUEUE_RECORDING_ENABLED'] = 'true'
-          ENV['KNAPSACK_PRO_QUEUE_ID'] = KnapsackPro::Config::EnvGenerator.set_queue_id
-
-          runner = new(KnapsackPro::Adapters::RSpecAdapter)
-
-          if runner.test_files_to_execute_exist?
-            require 'rspec/core/rake_task'
-
-            queue = Queue.new(runner.test_file_paths)
-            run_tests(queue, args, runner, 0)
           end
         end
       end
