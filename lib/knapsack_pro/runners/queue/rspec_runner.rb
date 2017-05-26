ce11ea1c905d78a92fd4be45d1f0222e50232b04
ArturT <arturtrzop@gmail.com>
2017-05-26 12:53:32 +0200

    Keep explicitly overridden ENVs to ensure the tests work

--- lib/knapsack_pro/runners/queue/rspec_runner.rb
+++ lib/knapsack_pro/runners/queue/rspec_runner.rb
@@ -9,7 +9,7 @@ module KnapsackPro
 
           ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN'] = KnapsackPro::Config::Env.test_suite_token_rspec
           ENV['KNAPSACK_PRO_QUEUE_RECORDING_ENABLED'] = 'true'
-          KnapsackPro::Config::EnvGenerator.set_queue_id
+          ENV['KNAPSACK_PRO_QUEUE_ID'] = KnapsackPro::Config::EnvGenerator.set_queue_id
 
           runner = new(KnapsackPro::Adapters::RSpecAdapter)
 
@@ -38,7 +38,8 @@ module KnapsackPro
             KnapsackPro::Report.save_node_queue_to_api
             exit(exitstatus)
           else
-            KnapsackPro::Config::EnvGenerator.set_subset_queue_id
+            subset_queue_id = KnapsackPro::Config::EnvGenerator.set_subset_queue_id
+            ENV['KNAPSACK_PRO_SUBSET_QUEUE_ID'] = subset_queue_id
 
             all_test_file_paths += test_file_paths
             cli_args = args + test_file_paths
