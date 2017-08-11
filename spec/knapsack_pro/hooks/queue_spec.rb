311febc7e88eb850f87328f3fca75a0f33d53262
ArturT <arturtrzop@gmail.com>
2017-08-11 20:56:35 +0200

    Defined methods for before and after queue hooks

--- spec/knapsack_pro/hooks/queue_spec.rb
+++ spec/knapsack_pro/hooks/queue_spec.rb
@@ -1,4 +1,30 @@
 describe KnapsackPro::Hooks::Queue do
+  describe '.call_before_queue' do
+    subject { described_class.call_before_queue }
+
+    context 'when callback is not set' do
+      before do
+        described_class.reset_before_queue
+      end
+
+      it { should be_nil }
+    end
+
+    context 'when callback is set' do
+      let(:queue_id) { double }
+
+      before do
+        expect(KnapsackPro::Config::Env).to receive(:queue_id).and_return(queue_id)
+
+        described_class.before_queue do |q_id|
+          [:fake_value, q_id]
+        end
+      end
+
+      it { should eq [:fake_value, queue_id] }
+    end
+  end
+
   describe '.call_after_subset_queue' do
     subject { described_class.call_after_subset_queue }
 
@@ -24,6 +50,32 @@ describe KnapsackPro::Hooks::Queue do
       end
 
       it { should eq [:fake_value, queue_id, subset_queue_id] }
+    end
+  end
+
+  describe '.call_after_queue' do
+    subject { described_class.call_after_queue }
+
+    context 'when callback is not set' do
+      before do
+        described_class.reset_after_queue
+      end
+
+      it { should be_nil }
+    end
+
+    context 'when callback is set' do
+      let(:queue_id) { double }
+
+      before do
+        expect(KnapsackPro::Config::Env).to receive(:queue_id).and_return(queue_id)
+
+        described_class.after_queue do |q_id|
+          [:fake_value, q_id]
+        end
+      end
+
+      it { should eq [:fake_value, queue_id] }
     end
   end
 end
