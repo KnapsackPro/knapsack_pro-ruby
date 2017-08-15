12423e3db56f778225cbee0b459d003f579b3964
ArturT <arturtrzop@gmail.com>
2017-08-15 13:48:24 +0200

    If fallback was activated then return empty list of tests for second run of the test_file_paths to stop the queue.

--- lib/knapsack_pro/queue_allocator.rb
+++ lib/knapsack_pro/queue_allocator.rb
@@ -9,6 +9,7 @@ module KnapsackPro
     end
 
     def test_file_paths(can_initialize_queue, executed_test_files)
+      return [] if @fallback_activated
       action = build_action(can_initialize_queue)
       connection = KnapsackPro::Client::Connection.new(action)
       response = connection.call
@@ -16,6 +17,7 @@ module KnapsackPro
         raise ArgumentError.new(response) if connection.errors?
         prepare_test_files(response)
       else
+        @fallback_activated = true
         KnapsackPro.logger.warn("Fallback mode started. We could not connect with Knapsack Pro API. Your tests will be executed based on directory names. If other CI nodes were able to connect with Knapsack Pro API then you may notice that some of the test files will be executed twice across CI nodes. The most important thing is to guarantee each of test files is run at least once! Read more about fallback mode at https://github.com/KnapsackPro/knapsack_pro-ruby")
         fallback_test_files(executed_test_files)
       end
