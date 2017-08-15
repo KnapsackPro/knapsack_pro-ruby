86479a789ce758e57bbba4f267113a1b8f43cbb6
ArturT <arturtrzop@gmail.com>
2017-08-15 15:15:52 +0200

    Update readme about fallback behaviour

--- lib/knapsack_pro/allocator.rb
+++ lib/knapsack_pro/allocator.rb
@@ -14,6 +14,7 @@ module KnapsackPro
         raise ArgumentError.new(response) if connection.errors?
         prepare_test_files(response)
       else
+        KnapsackPro.logger.warn("Fallback mode started. We could not connect with Knapsack Pro API. Your tests will be executed based on directory names. Read more about fallback mode at https://github.com/KnapsackPro/knapsack_pro-ruby")
         fallback_test_files
       end
     end
