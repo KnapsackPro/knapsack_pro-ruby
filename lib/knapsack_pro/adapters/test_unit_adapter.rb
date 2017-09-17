66f3264eec0b78bc4f2a71d9c7046b4e9fa85a50
ArturT <arturtrzop@gmail.com>
2017-09-17 22:06:02 +0200

    Bind methods to show summary and save report for unit-test adapter.
    TODO: figure out how to track test files time execution

--- lib/knapsack_pro/adapters/test_unit_adapter.rb
+++ lib/knapsack_pro/adapters/test_unit_adapter.rb
@@ -4,49 +4,52 @@ module KnapsackPro
       TEST_DIR_PATTERN = 'test/**{,/*/**}/*_test.rb'
 
       def self.test_path(example_group)
-        if defined?(Turnip) && Turnip::VERSION.to_i < 2
-          unless example_group[:turnip]
-            until example_group[:parent_example_group].nil?
-              example_group = example_group[:parent_example_group]
-            end
-          end
-        else
-          until example_group[:parent_example_group].nil?
-            example_group = example_group[:parent_example_group]
-          end
-        end
+        #if defined?(Turnip) && Turnip::VERSION.to_i < 2
+          #unless example_group[:turnip]
+            #until example_group[:parent_example_group].nil?
+              #example_group = example_group[:parent_example_group]
+            #end
+          #end
+        #else
+          #until example_group[:parent_example_group].nil?
+            #example_group = example_group[:parent_example_group]
+          #end
+        #end
 
-        example_group[:file_path]
+        #example_group[:file_path]
       end
 
       def bind_time_tracker
-        ::RSpec.configure do |config|
-          config.prepend_before(:each) do
-            current_example_group =
-              if ::RSpec.respond_to?(:current_example)
-                ::RSpec.current_example.metadata[:example_group]
-              else
-                example.metadata
-              end
-            KnapsackPro.tracker.current_test_path = KnapsackPro::Adapters::RSpecAdapter.test_path(current_example_group)
-            KnapsackPro.tracker.start_timer
-          end
+        #Test::Unit.at_start do |a, b|
+          #require 'pry'; binding.pry
+          ##KnapsackPro.tracker.current_test_path = KnapsackPro::Adapters::RSpecAdapter.test_path(current_example_group)
+          ##KnapsackPro.tracker.start_timer
+        #end
+        #::RSpec.configure do |config|
+          #config.prepend_before(:each) do
+            #current_example_group =
+              #if ::RSpec.respond_to?(:current_example)
+                #::RSpec.current_example.metadata[:example_group]
+              #else
+                #example.metadata
+              #end
+            #KnapsackPro.tracker.current_test_path = KnapsackPro::Adapters::RSpecAdapter.test_path(current_example_group)
+            #KnapsackPro.tracker.start_timer
+          #end
 
-          config.append_after(:each) do
-            KnapsackPro.tracker.stop_timer
-          end
+          #config.append_after(:each) do
+            #KnapsackPro.tracker.stop_timer
+          #end
+        #end
 
-          config.after(:suite) do
-            KnapsackPro.logger.debug(KnapsackPro::Presenter.global_time)
-          end
+        Test::Unit.at_exit do
+          KnapsackPro.logger.debug(KnapsackPro::Presenter.global_time)
         end
       end
 
       def bind_save_report
-        ::RSpec.configure do |config|
-          config.after(:suite) do
-            KnapsackPro::Report.save
-          end
+        Test::Unit.at_exit do
+          KnapsackPro::Report.save
         end
       end
     end
