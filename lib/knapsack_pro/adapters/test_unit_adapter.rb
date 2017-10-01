0803311146c5e984788270e48c8d07218ef1f03f
ArturT <arturtrzop@gmail.com>
2017-10-01 19:22:29 +0200

    TestUnit adapter works.

--- lib/knapsack_pro/adapters/test_unit_adapter.rb
+++ lib/knapsack_pro/adapters/test_unit_adapter.rb
@@ -2,43 +2,47 @@ module KnapsackPro
   module Adapters
     class TestUnitAdapter < BaseAdapter
       TEST_DIR_PATTERN = 'test/**{,/*/**}/*_test.rb'
+      @@parent_of_test_dir = nil
 
-      def self.test_path(example_group)
-        #if defined?(Turnip) && Turnip::VERSION.to_i < 2
-          #unless example_group[:turnip]
-            #until example_group[:parent_example_group].nil?
-              #example_group = example_group[:parent_example_group]
-            #end
-          #end
-        #else
-          #until example_group[:parent_example_group].nil?
-            #example_group = example_group[:parent_example_group]
-          #end
-        #end
-
-        #example_group[:file_path]
+      def self.test_path(obj)
+        first_test = obj.tests.first
+        method = first_test.method_name
+        full_test_path = first_test.method(method).source_location.first
+        parent_of_test_dir_regexp = Regexp.new("^#{@@parent_of_test_dir}")
+        test_path = full_test_path.gsub(parent_of_test_dir_regexp, '.')
+        # test_path will look like ./test/dir/unit_test.rb
+        test_path
       end
 
-      # FIXME
+      # Overrides the method from unit-test gem
+      # https://github.com/test-unit/test-unit/blob/master/lib/test/unit/testsuite.rb
       module BindTimeTrackerTestUnitPlugin
-        def setup
-          super
-          puts '/'*10
-          puts 'SETUP plugin'
-          #KnapsackPro.tracker.current_test_path = KnapsackPro::Adapters::TestUnitAdapter.test_path(self)
-          #KnapsackPro.tracker.start_timer
+        def run_startup(result)
+          return if @test_case.nil?
+          KnapsackPro.tracker.current_test_path = KnapsackPro::Adapters::TestUnitAdapter.test_path(self)
+          KnapsackPro.tracker.start_timer
+          return if !@test_case.respond_to?(:startup)
+          begin
+            @test_case.startup
+          rescue Exception
+            raise unless handle_exception($!, result)
+          end
         end
 
-        def teardown
-          #KnapsackPro.tracker.stop_timer
-          puts '/'*10
-          puts 'SETUP teardown'
-          super
+        def run_shutdown(result)
+          return if @test_case.nil?
+          KnapsackPro.tracker.stop_timer
+          return if !@test_case.respond_to?(:shutdown)
+          begin
+            @test_case.shutdown
+          rescue Exception
+            raise unless handle_exception($!, result)
+          end
         end
       end
 
       def bind_time_tracker
-        Test::Unit::TestCase.send(:include, BindTimeTrackerTestUnitPlugin)
+        Test::Unit::TestSuite.send(:prepend, BindTimeTrackerTestUnitPlugin)
 
         Test::Unit.at_exit do
           KnapsackPro.logger.debug(KnapsackPro::Presenter.global_time)
@@ -49,6 +53,11 @@ module KnapsackPro
         Test::Unit.at_exit do
           KnapsackPro::Report.save
         end
+      end
+
+      def set_test_helper_path(file_path)
+        test_dir_path = File.dirname(file_path)
+        @@parent_of_test_dir = File.expand_path('../', test_dir_path)
       end
     end
   end
