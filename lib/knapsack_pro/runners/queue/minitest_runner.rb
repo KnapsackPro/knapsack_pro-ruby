14a7cd27879ed852ff6f10a8ffd0ee8bb1a13514
ArturT <arturtrzop@gmail.com>
2018-08-02 15:55:18 +0200

    Use while instead of recursion for queue mode in minitest

--- lib/knapsack_pro/runners/queue/minitest_runner.rb
+++ lib/knapsack_pro/runners/queue/minitest_runner.rb
@@ -17,10 +17,29 @@ module KnapsackPro
           $LOAD_PATH.unshift(runner.test_dir)
 
           cli_args = (args || '').split
-          run_tests(runner, true, cli_args, 0, [])
+
+          accumulator = {
+            status: :next,
+            runner: runner,
+            can_initialize_queue: true,
+            args: cli_args,
+            exitstatus: 0,
+            all_test_file_paths: [],
+          }
+          while accumulator[:status] == :next
+            accumulator = run_tests(accumulator)
+          end
+
+          Kernel.exit(accumulator[:exitstatus])
         end
 
-        def self.run_tests(runner, can_initialize_queue, args, exitstatus, all_test_file_paths)
+        def self.run_tests(accumulator)
+          runner = accumulator.fetch(:runner)
+          can_initialize_queue = accumulator.fetch(:can_initialize_queue)
+          args = accumulator.fetch(:args)
+          exitstatus = accumulator.fetch(:exitstatus)
+          all_test_file_paths = accumulator.fetch(:all_test_file_paths)
+
           test_file_paths = runner.test_file_paths(
             can_initialize_queue: can_initialize_queue,
             executed_test_files: all_test_file_paths
@@ -30,7 +49,11 @@ module KnapsackPro
             KnapsackPro::Hooks::Queue.call_after_queue
 
             KnapsackPro::Report.save_node_queue_to_api
-            exit(exitstatus)
+
+            return {
+              status: :completed,
+              exitstatus: exitstatus,
+            }
           else
             subset_queue_id = KnapsackPro::Config::EnvGenerator.set_subset_queue_id
             ENV['KNAPSACK_PRO_SUBSET_QUEUE_ID'] = subset_queue_id
@@ -46,7 +69,14 @@ module KnapsackPro
 
             KnapsackPro::Report.save_subset_queue_to_file
 
-            run_tests(runner, false, args, exitstatus, all_test_file_paths)
+            return {
+              status: :next,
+              runner: runner,
+              can_initialize_queue: false,
+              args: args,
+              exitstatus: exitstatus,
+              all_test_file_paths: all_test_file_paths,
+            }
           end
         end
 
