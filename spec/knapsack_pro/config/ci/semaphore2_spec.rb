683ced6b015d798978d1b401345dfc3b35c0996b
Artur Trzop <arturtrzop@gmail.com>
2019-08-23 14:26:42 +0200

    Add support for job index and job count for parallelism in Semaphore 2.0

--- spec/knapsack_pro/config/ci/semaphore2_spec.rb
+++ spec/knapsack_pro/config/ci/semaphore2_spec.rb
@@ -10,13 +10,27 @@ describe KnapsackPro::Config::CI::Semaphore2 do
   describe '#node_total' do
     subject { described_class.new.node_total }
 
-    it { should be nil }
+    context 'when environment exists' do
+      let(:env) { { 'SEMAPHORE_JOB_COUNT' => 4 } }
+      it { should eql 4 }
+    end
+
+    context "when environment doesn't exist" do
+      it { should be nil }
+    end
   end
 
   describe '#node_index' do
     subject { described_class.new.node_index }
 
-    it { should be nil }
+    context 'when environment exists' do
+      let(:env) { { 'SEMAPHORE_JOB_INDEX' => 4 } }
+      it { should eql 3 }
+    end
+
+    context "when environment doesn't exist" do
+      it { should be nil }
+    end
   end
 
   describe '#node_build_id' do
