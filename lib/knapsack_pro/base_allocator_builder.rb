2c6e971f38a246c686357de4f719d215510ae245
Artur Trzop <arturtrzop@gmail.com>
2020-04-05 14:31:10 +0200

    Make test_files method public and add spec for it for BaseAllocatorBuilder

--- lib/knapsack_pro/base_allocator_builder.rb
+++ lib/knapsack_pro/base_allocator_builder.rb
@@ -21,22 +21,6 @@ module KnapsackPro
       KnapsackPro::Config::Env.test_dir || TestFilePattern.test_dir(adapter_class)
     end
 
-    private
-
-    attr_reader :adapter_class
-
-    def env
-      KnapsackPro::Config::Env
-    end
-
-    def repository_adapter
-      KnapsackPro::RepositoryAdapterInitiator.call
-    end
-
-    def test_file_pattern
-      TestFilePattern.call(adapter_class)
-    end
-
     def test_files
       found_test_files = KnapsackPro::TestFileFinder.call(test_file_pattern)
 
@@ -61,6 +45,22 @@ module KnapsackPro
       else
         found_test_files
       end
+    end
+
+    private
+
+    attr_reader :adapter_class
+
+    def env
+      KnapsackPro::Config::Env
+    end
+
+    def repository_adapter
+      KnapsackPro::RepositoryAdapterInitiator.call
+    end
+
+    def test_file_pattern
+      TestFilePattern.call(adapter_class)
     end
   end
 end
