053b895fc788a5153e8c319b3e67016b79b37e30
Artur Trzop <arturtrzop@gmail.com>
2020-05-02 16:38:45 +0200

    Add prefix RACK_ENV=test RAILS_ENV=test to bundle exec rake knapsack_pro:rspec_test_example_detector to make sure the rake task is loaded properly

--- lib/knapsack_pro/base_allocator_builder.rb
+++ lib/knapsack_pro/base_allocator_builder.rb
@@ -43,7 +43,7 @@ module KnapsackPro
         KnapsackPro.logger.warn("Generating RSpec test examples JSON report for slow test files to prepare it to be split by test examples (by individual 'it's. Thanks to that a single slow test file can be split across parallel CI nodes). Analyzing #{slow_test_files.size} slow test files.")
 
         # generate RSpec JSON report in separate process to not pollute RSpec state
-        cmd = 'bundle exec rake knapsack_pro:rspec_test_example_detector'
+        cmd = 'RACK_ENV=test RAILS_ENV=test bundle exec rake knapsack_pro:rspec_test_example_detector'
         unless Kernel.system(cmd)
           raise "Could not generate JSON report for RSpec. Rake task failed when running #{cmd}"
         end
