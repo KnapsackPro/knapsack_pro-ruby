fd499982207b707675c02527abfd0a9580f67b8e
Artur Trzop <arturtrzop@gmail.com>
2020-05-05 17:51:14 +0200

    Update base_allocator_builder_spec.rb

--- spec/knapsack_pro/base_allocator_builder_spec.rb
+++ spec/knapsack_pro/base_allocator_builder_spec.rb
@@ -129,10 +129,10 @@ describe KnapsackPro::BaseAllocatorBuilder do
       end
     end
 
-    context 'when RSpec adapter and rspec split by test examples is enabled' do
+    context 'when RSpec adapter AND rspec split by test examples is enabled' do
       let(:adapter_class) { KnapsackPro::Adapters::RSpecAdapter }
-      let(:test_files) { double(size: 1000) }
-      let(:cmd) { 'bundle exec rake knapsack_pro:rspec_test_example_detector' }
+      let(:test_files_to_run) { double }
+      let(:cmd) { 'RACK_ENV=test RAILS_ENV=test bundle exec rake knapsack_pro:rspec_test_example_detector' }
 
       before do
         expect(KnapsackPro::Config::Env).to receive(:rspec_split_by_test_examples?).and_return(true)
@@ -140,38 +140,91 @@ describe KnapsackPro::BaseAllocatorBuilder do
         test_file_pattern = double
         expect(KnapsackPro::TestFilePattern).to receive(:call).with(adapter_class).and_return(test_file_pattern)
 
-        expect(KnapsackPro::TestFileFinder).to receive(:call).with(test_file_pattern).and_return(test_files)
+        expect(KnapsackPro::TestFileFinder).to receive(:call).with(test_file_pattern).and_return(test_files_to_run)
+      end
 
-        expect(Kernel).to receive(:system).with(cmd).and_return(cmd_result)
+      context 'when RSpec version < 3.3.0' do
+        before do
+          stub_const('RSpec::Core::Version::STRING', '3.2.0')
+        end
+
+        it do
+          expect { subject }.to raise_error RuntimeError, 'RSpec >= 3.3.0 is required to split test files by test examples. Learn more: https://github.com/KnapsackPro/knapsack_pro-ruby#split-test-files-by-test-cases'
+        end
       end
 
       context 'when rake task to detect RSpec test examples works' do
+        let(:slow_test_files) { double(size: 5) }
         let(:cmd_result) { true }
         let(:test_file_example_paths) { double }
         let(:logger) { instance_double(Logger) }
+        let(:test_files_with_test_cases) { double }
 
         before do
+          expect(allocator_builder).to receive(:get_slow_test_files).and_return(slow_test_files)
+
+          expect(KnapsackPro).to receive(:logger).and_return(logger)
+
+          expect(Kernel).to receive(:system).with(cmd).and_return(cmd_result)
+
           rspec_test_example_detector = instance_double(KnapsackPro::TestCaseDetectors::RSpecTestExampleDetector)
           expect(KnapsackPro::TestCaseDetectors::RSpecTestExampleDetector).to receive(:new).and_return(rspec_test_example_detector)
           expect(rspec_test_example_detector).to receive(:test_file_example_paths).and_return(test_file_example_paths)
 
-          expect(KnapsackPro).to receive(:logger).at_least(1).and_return(logger)
+          expect(KnapsackPro::TestFilesWithTestCasesComposer).to receive(:call).with(test_files_to_run, slow_test_files, test_file_example_paths).and_return(test_files_with_test_cases)
         end
 
         it do
-          expect(logger).to receive(:info).with("Generating RSpec test examples JSON report for slow test files to prepare it to be split by test examples (by individual 'it's. Thanks to that a single slow test file can be split across parallel CI nodes). Analyzing 1000 slow test files.")
+          expect(logger).to receive(:info).with("Generating RSpec test examples JSON report for slow test files to prepare it to be split by test examples (by individual 'it's. Thanks to that a single slow test file can be split across parallel CI nodes). Analyzing 5 slow test files.")
 
-          expect(subject).to eq test_file_example_paths
+          expect(subject).to eq test_files_with_test_cases
         end
       end
 
       context 'when rake task to detect RSpec test examples failed' do
+        let(:slow_test_files) { double(size: 5) }
         let(:cmd_result) { false }
 
+        before do
+          expect(allocator_builder).to receive(:get_slow_test_files).and_return(slow_test_files)
+
+          expect(Kernel).to receive(:system).with(cmd).and_return(cmd_result)
+        end
+
         it do
-          expect { subject }.to raise_error(RuntimeError, 'Could not generate JSON report for RSpec. Rake task failed when running bundle exec rake knapsack_pro:rspec_test_example_detector')
+          expect { subject }.to raise_error(RuntimeError, 'Could not generate JSON report for RSpec. Rake task failed when running RACK_ENV=test RAILS_ENV=test bundle exec rake knapsack_pro:rspec_test_example_detector')
         end
       end
+    end
+  end
+
+  describe 'private #get_slow_test_files' do
+    subject { allocator_builder.send(:get_slow_test_files) }
+
+    before do
+      expect(KnapsackPro::Config::Env).to receive(:slow_test_file_pattern).and_return(slow_test_file_pattern)
+    end
+
+    context 'when slow test file pattern is present' do
+      let(:slow_test_files) { double(:slow_test_files_based_on_pattern, size: 3) }
+      let(:slow_test_file_pattern) { double }
+
+      before do
+        expect(KnapsackPro::TestFileFinder).to receive(:slow_test_files_by_pattern).with(adapter_class).and_return(slow_test_files)
+      end
+
+      it { expect(subject).to eq slow_test_files }
+    end
+
+    context 'when slow test file pattern is not present' do
+      let(:slow_test_files) { double(:slow_test_files_based_on_api, size: 2) }
+      let(:slow_test_file_pattern) { nil }
+
+      before do
+        expect(KnapsackPro::SlowTestFileFinder).to receive(:call).with(adapter_class).and_return(slow_test_files)
+      end
+
+      it { expect(subject).to eq slow_test_files }
     end
   end
 end
