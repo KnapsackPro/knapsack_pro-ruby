5b97ad6bb933f287c1fae6a53c44eed5be2b7987
Feng Sha <feng.sha@fullscript.com>
2020-11-03 15:33:04 -0500

    Revert "remove , from end of the test file path"
    This reverts commit 77320671361585abd43ef793a9a20628f51c87ba.

--- spec/knapsack_pro/test_file_cleaner_spec.rb
+++ spec/knapsack_pro/test_file_cleaner_spec.rb
@@ -1,19 +1,11 @@
 describe KnapsackPro::TestFileCleaner do
   describe '.clean' do
+    let(:test_file_path) { './models/user_spec.rb' }
+
     subject { described_class.clean(test_file_path) }
 
-    context "removes ./ " do
-      let(:test_file_path) { './models/user_spec.rb' }
-      it 'removes ./ from the begining of the test file path' do
-        expect(subject).to eq 'models/user_spec.rb'
-      end
-    end
-
-    context "removes , " do
-      let(:test_file_path) { 'models/user_spec.rb,' }
-      it 'removes , from the end of the test file path' do
-        expect(subject).to eq 'models/user_spec.rb'
-      end
+    it 'removes ./ from the begining of the test file path' do
+      expect(subject).to eq 'models/user_spec.rb'
     end
   end
 end
