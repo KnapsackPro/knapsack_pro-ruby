90351bfc19d2970aa103b0e5138d4969d20731c3
Artur Trzop <arturtrzop@gmail.com>
2021-01-04 14:35:13 +0100

    Show 2 reasons why tests could not be recorded properly

--- lib/knapsack_pro/adapters/base_adapter.rb
+++ lib/knapsack_pro/adapters/base_adapter.rb
@@ -34,7 +34,9 @@ module KnapsackPro
             File.delete(ADAPTER_BIND_METHOD_CALLED_FILE)
           else
             KnapsackPro.logger.error('-'*10 + ' Configuration error ' + '-'*50)
-            raise "You forgot to call #{self}.bind method for your test runner to record test files time execution. Please follow installation guide to configure your project properly https://docs.knapsackpro.com/knapsack_pro-ruby/guide/"
+            KnapsackPro.logger.error("You forgot to call #{self}.bind method for your test runner to record test files time execution. Please follow the installation guide to configure your project properly https://docs.knapsackpro.com/knapsack_pro-ruby/guide/")
+            KnapsackPro.logger.error("If you already have #{self}.bind method added and you still see this error then one of your tests must have to delete tmp/knapsack_pro directory from the disk accidentally. Please ensure you do not remove tmp/knapsack_pro directory: https://knapsackpro.com/faq/question/why-all-test-files-have-01s-time-execution-for-my-ci-build-in-user-dashboard")
+            raise "There is an error in your project configuration. Please read above error message."
           end
         end
       end
