d24df087256c0f1e29960c9bfd68a7e2ea994f22
Artur Trzop <arturtrzop@gmail.com>
2021-05-24 14:48:53 +0200

    try to fix git adapter spec on CI

--- spec/knapsack_pro/repository_adapters/git_adapter_spec.rb
+++ spec/knapsack_pro/repository_adapters/git_adapter_spec.rb
@@ -2,33 +2,54 @@ describe KnapsackPro::RepositoryAdapters::GitAdapter do
   let!(:circle_sha1) { ENV['CIRCLE_SHA1'] }
   let!(:circle_branch) { ENV['CIRCLE_BRANCH'] }
 
-  before do
-    stub_const('ENV', {
-      'KNAPSACK_PRO_PROJECT_DIR' => KnapsackPro.root,
-    })
-  end
-
   it { should be_kind_of KnapsackPro::RepositoryAdapters::BaseAdapter }
 
   describe '#commit_hash' do
     subject { described_class.new.commit_hash }
 
-    it { should_not be_nil }
-    its(:size) { should eq 40 }
+    context do
+      before do
+        stub_const('ENV', {
+          'KNAPSACK_PRO_PROJECT_DIR' => KnapsackPro.root,
+        })
+      end
+
+      it { should_not be_nil }
+      its(:size) { should eq 40 }
+    end
+
     it { should eq circle_sha1 } if ENV['CIRCLECI']
   end
 
   describe '#branch' do
     subject { described_class.new.branch }
 
-    it { should_not be_nil }
+    context do
+      before do
+        stub_const('ENV', {
+          'KNAPSACK_PRO_PROJECT_DIR' => KnapsackPro.root,
+        })
+      end
+
+      it { should_not be_nil }
+    end
+
     it { should eq circle_branch } if ENV['CIRCLECI']
   end
 
   describe '#branches' do
     subject { described_class.new.branches }
 
-    it { expect(subject.include?('master')).to be true }
+    context do
+      before do
+        stub_const('ENV', {
+          'KNAPSACK_PRO_PROJECT_DIR' => KnapsackPro.root,
+        })
+      end
+
+      it { expect(subject.include?('master')).to be true }
+    end
+
     it { expect(subject.include?(circle_branch)).to be true } if ENV['CIRCLECI']
   end
 end
