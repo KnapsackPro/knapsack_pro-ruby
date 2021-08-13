390a1d3db5ed94c297c15579821ee54dface97f4
Artur Trzop <arturtrzop@gmail.com>
2021-08-13 15:42:57 +0200

    add potential word

--- spec/knapsack_pro/report_spec.rb
+++ spec/knapsack_pro/report_spec.rb
@@ -104,7 +104,7 @@ describe KnapsackPro::Report do
         expect(logger).to receive(:warn).with('2 test files were executed on this CI node but the recorded time was lost due to:')
         expect(logger).to receive(:warn).with('1. Please ensure you do not remove the contents of the .knapsack_pro directory between tests run.')
         expect(logger).to receive(:warn).with("2. Ensure you've added Knapsack::Adapters::RSpecAdapter.bind in your rails_helper.rb or spec_helper.rb. Please follow the installation guide again: https://docs.knapsackpro.com/integration/")
-        expect(logger).to receive(:warn).with('3. Another reason for this warning is that all your tests are empty test files, pending tests, or they have syntax errors, and the time execution was not recorded for them.')
+        expect(logger).to receive(:warn).with('3. Another potential reason for this warning is that all your tests are empty test files, pending tests, or they have syntax errors, and the time execution was not recorded for them.')
 
         expect(described_class).to receive(:create_build_subset).with(
           json_test_file_a + json_test_file_b
