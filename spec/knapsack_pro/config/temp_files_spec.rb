5d6e5b95c6c34e07afddce1b3ed5443bd57e8bcd
Artur Trzop <arturtrzop@gmail.com>
2021-08-17 11:46:31 +0200

    verify asterisk is present in gitignore file

--- spec/knapsack_pro/config/temp_files_spec.rb
+++ spec/knapsack_pro/config/temp_files_spec.rb
@@ -17,6 +17,9 @@ describe KnapsackPro::Config::TempFiles, :clear_tmp do
     it '.gitignore file has content' do
       subject
       expect(File.read(gitignore_file_path)).to include '# This directory is used by knapsack_pro gem for storing temporary files during tests runtime'
+      expect(File.read(gitignore_file_path)).to include '# Ignore all files, and do not commit this directory into your repository.'
+      expect(File.read(gitignore_file_path)).to include '# Learn more at https://knapsackpro.com'
+      expect(File.read(gitignore_file_path)).to include '*'
     end
   end
 end
