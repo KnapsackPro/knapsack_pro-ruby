0bc543a5e85adc39ed5b5e7fec0738c9f764b5ef
Artur Trzop <arturtrzop@gmail.com>
2023-09-19 19:24:05 +0200

    feat(performance): If no slow test files detected then don't generate RSpec json report with test examples

--- lib/knapsack_pro/base_allocator_builder.rb
+++ lib/knapsack_pro/base_allocator_builder.rb
@@ -42,28 +42,14 @@ module KnapsackPro
         end
 
         slow_test_files = get_slow_test_files
+        return test_files_to_run if slow_test_files.empty?
 
-        KnapsackPro.logger.info("Generating RSpec test examples JSON report for slow test files to prepare it to be split by test examples (by individual 'it's. Thanks to that a single slow test file can be split across parallel CI nodes). Analyzing #{slow_test_files.size} slow test files.")
+        test_file_example_paths = rspec_test_file_example_paths_for_slow_test_files(slow_test_files)
 
-        # generate RSpec JSON report in separate process to not pollute RSpec state
-        cmd = [
-          'RACK_ENV=test',
-          'RAILS_ENV=test',
-          KnapsackPro::Config::Env.rspec_test_example_detector_prefix,
-          'rake knapsack_pro:rspec_test_example_detector',
-        ].join(' ')
-        unless Kernel.system(cmd)
-          raise "Could not generate JSON report for RSpec. Rake task failed when running #{cmd}"
-        end
-
-        # read JSON report
-        detector = KnapsackPro::TestCaseDetectors::RSpecTestExampleDetector.new
-        test_file_example_paths = detector.test_file_example_paths
-
-        KnapsackPro::TestFilesWithTestCasesComposer.call(test_files_to_run, slow_test_files, test_file_example_paths)
-      else
-        test_files_to_run
+        return KnapsackPro::TestFilesWithTestCasesComposer.call(test_files_to_run, slow_test_files, test_file_example_paths)
       end
+
+      test_files_to_run
     end
 
     private
@@ -100,6 +86,25 @@ module KnapsackPro
         end
       KnapsackPro.logger.debug("Detected #{slow_test_files.size} slow test files: #{slow_test_files.inspect}")
       slow_test_files
+    end
+
+    def rspec_test_file_example_paths_for_slow_test_files(slow_test_files)
+      KnapsackPro.logger.info("Generating RSpec test examples JSON report for slow test files to prepare it to be split by test examples (by individual 'it's. Thanks to that a single slow test file can be split across parallel CI nodes). Analyzing #{slow_test_files.size} slow test files.")
+
+      # generate the RSpec JSON report in a separate process to not pollute the RSpec state
+      cmd = [
+        'RACK_ENV=test',
+        'RAILS_ENV=test',
+        KnapsackPro::Config::Env.rspec_test_example_detector_prefix,
+        'rake knapsack_pro:rspec_test_example_detector',
+      ].join(' ')
+      unless Kernel.system(cmd)
+        raise "Could not generate JSON report for RSpec. Rake task failed when running #{cmd}"
+      end
+
+      # read the JSON report
+      detector = KnapsackPro::TestCaseDetectors::RSpecTestExampleDetector.new
+      test_file_example_paths = detector.test_file_example_paths
     end
   end
 end
