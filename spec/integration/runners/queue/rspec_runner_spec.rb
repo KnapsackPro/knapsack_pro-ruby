d65b7dd5a7ffe4525edbbfec6b8b4d9670020c9b
Artur Trzop <arturtrzop@gmail.com>
2024-01-29 22:08:33 +0100

    Update rspec_runner_spec.rb

--- spec/integration/runners/queue/rspec_runner_spec.rb
+++ spec/integration/runners/queue/rspec_runner_spec.rb
@@ -73,27 +73,81 @@ describe "#{KnapsackPro::Runners::Queue::RSpecRunner} - Integration tests" do
     ENV['TEST__SHOW_DEBUG_LOG'] = 'true'
   end
 
-  context 'context' do
+  context 'when a few batches of tests returned by the Queue API' do
+    let(:rspec_options) { '--format d' }
+
     it do
       spec_a = SpecItem.new(
         'a_spec.rb',
         <<~SPEC
-        describe "A" do
-          it 'test case' do
+        describe "A_describe" do
+          it 'A1 test example' do
             expect(1).to eq 1
           end
         end
         SPEC
       )
 
-      rspec_options = '--format d'
-      run_specs(spec_helper_with_knapsack, rspec_options, [spec_a]) do
+      spec_b = SpecItem.new(
+        'b_spec.rb',
+        <<~SPEC
+        describe "B_describe" do
+          it 'B1 test example' do
+            expect(1).to eq 1
+          end
+        end
+        SPEC
+      )
+
+      spec_c = SpecItem.new(
+        'c_spec.rb',
+        <<~SPEC
+        describe "C_describe" do
+          it 'C1 test example' do
+            expect(1).to eq 1
+          end
+        end
+        SPEC
+      )
+
+      run_specs(spec_helper_with_knapsack, rspec_options, [
+        spec_a,
+        spec_b,
+        spec_c
+      ]) do
         mock_batched_tests([
-          [spec_a.path],
+          [spec_a.path, spec_b.path],
+          [spec_c.path],
         ])
 
         result = subject
 
+        # expect RSpecAdapter.bind method to be called for Queue Mode
+        # expect logger to print to stdout
+        expect(result.stdout).to include('DEBUG -- : [knapsack_pro] Test suite time execution queue recording enabled.')
+
+        # expect to execute test examples from all batches
+        # expect output to be a documentation when the --format option is provided
+        expect(result.stdout).to include('A1 test example')
+        expect(result.stdout).to include('B1 test example')
+        expect(result.stdout).to include('C1 test example')
+
+        # expect copy & paste command to reproduce tests for each batch of tests
+        expect(result.stdout).to include('INFO -- : [knapsack_pro] To retry the last batch of tests fetched from the Queue API, please run the following command on your machine:')
+        expect(result.stdout).to include('INFO -- : [knapsack_pro] bundle exec rspec --format d --default-path spec_integration "spec_integration/a_spec.rb" "spec_integration/b_spec.rb"')
+        expect(result.stdout).to include('INFO -- : [knapsack_pro] bundle exec rspec --format d --default-path spec_integration "spec_integration/c_spec.rb"')
+
+        # expect copy & paste command to reproduce tests for the CI node
+        expect(result.stdout).to include('INFO -- : [knapsack_pro] To retry all the tests assigned to this CI node, please run the following command on your machine:')
+        expect(result.stdout).to include('INFO -- : [knapsack_pro] bundle exec rspec --format d --default-path spec_integration "spec_integration/a_spec.rb" "spec_integration/b_spec.rb" "spec_integration/c_spec.rb"')
+
+        # expect RSpec to show a summary with a correct number of test examples
+        expect(result.stdout).to include('3 examples, 0 failures')
+
+        # expect Knapsack Pro presenter with global time
+        expect(result.stdout).to include('DEBUG -- : [knapsack_pro] Global time execution for tests:')
+
+        # expect successful tests
         expect(result.exit_code).to eq 0
       end
     end
