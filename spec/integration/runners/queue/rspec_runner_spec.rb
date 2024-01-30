a82afd8420d710780884e25db70b9dc1c5fc7adb
Artur Trzop <arturtrzop@gmail.com>
2024-01-30 17:09:46 +0100

    Update rspec_runner_spec.rb

--- spec/integration/runners/queue/rspec_runner_spec.rb
+++ spec/integration/runners/queue/rspec_runner_spec.rb
@@ -322,4 +322,75 @@ describe "#{KnapsackPro::Runners::Queue::RSpecRunner} - Integration tests" do
       end
     end
   end
+
+  context 'when hooks are defined' do
+    it 'RSpec before/after hooks are called only once for multiple batches of tests' do
+      rspec_options = ''
+
+      spec_helper_content = <<~SPEC
+      require 'knapsack_pro'
+      KnapsackPro::Adapters::RSpecAdapter.bind
+
+      RSpec.configure do |config|
+        config.before(:suite) do
+          puts "RSpec_before_suite_hook"
+        end
+        config.after(:suite) do
+          puts "RSpec_after_suite_hook"
+        end
+      end
+      SPEC
+
+      spec_a = SpecItem.new(
+        'a_spec.rb',
+        <<~SPEC
+        describe "A_describe" do
+          it 'A1 test example' do
+            expect(1).to eq 1
+          end
+        end
+        SPEC
+      )
+
+      spec_b = SpecItem.new(
+        'b_spec.rb',
+        <<~SPEC
+        describe "B_describe" do
+          it 'B1 test example' do
+            expect(1).to eq 1
+          end
+        end
+        SPEC
+      )
+
+      spec_c = SpecItem.new(
+        'c_spec.rb',
+        <<~SPEC
+        describe "C_describe" do
+          it 'C1 test example' do
+            expect(1).to eq 1
+          end
+        end
+        SPEC
+      )
+
+      run_specs(spec_helper_content, rspec_options, [
+        spec_a,
+        spec_b,
+        spec_c,
+      ]) do
+        mock_batched_tests([
+          [spec_a.path, spec_b.path],
+          [spec_c.path],
+        ])
+
+        result = subject
+
+        expect(result.stdout.scan(/RSpec_before_suite_hook/).size).to eq 1
+        expect(result.stdout.scan(/RSpec_after_suite_hook/).size).to eq 1
+
+        expect(result.exit_code).to eq 0
+      end
+    end
+  end
 end
