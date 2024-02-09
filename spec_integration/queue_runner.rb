86aa4c427d0736d21740075c43e67a7e4eb874bb
Artur Trzop <arturtrzop@gmail.com>
2024-02-09 22:37:17 +0100

    Update spec_integration/queue_runner.rb
    Co-authored-by: Riccardo <riccardo.odone@gmail.com>

--- spec_integration/queue_runner.rb
+++ spec_integration/queue_runner.rb
@@ -14,9 +14,8 @@ module KnapsackProExtensions
   module QueueAllocatorExtension
     def test_file_paths(can_initialize_queue, executed_test_files)
       @@batch_index ||= 0
-      batches = BATCHED_TESTS + [
-        [], # the last Queue API response is always an empty list of test files
-      ]
+      last_batch = []
+      batches = [*BATCHED_TESTS, last_batch]
       tests = batches[@@batch_index]
       @@batch_index += 1
 
