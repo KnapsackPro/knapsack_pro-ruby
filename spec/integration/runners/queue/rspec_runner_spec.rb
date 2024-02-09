ca22567973dcc02d1871e76b03d716d12da0dbc1
Artur Trzop <arturtrzop@gmail.com>
2024-02-09 22:19:39 +0100

    update(spec): remove KNAPSACK_PRO_LOG_LEVEL=info when it's not needed for the test (just use a default log level)

--- spec/integration/runners/queue/rspec_runner_spec.rb
+++ spec/integration/runners/queue/rspec_runner_spec.rb
@@ -262,14 +262,6 @@ describe "#{KnapsackPro::Runners::Queue::RSpecRunner} - Integration tests", :cle
   end
 
   context 'when RSpec options are not set' do
-    before do
-      ENV['KNAPSACK_PRO_LOG_LEVEL'] = 'info'
-    end
-
-    after do
-      ENV.delete('KNAPSACK_PRO_LOG_LEVEL')
-    end
-
     it 'uses a default progress formatter' do
       rspec_options = ''
 
