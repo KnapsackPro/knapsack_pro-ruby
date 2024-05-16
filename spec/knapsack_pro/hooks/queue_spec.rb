62020afcfaf1202c8d67c2e474eceb0b9a08bcaa
Artur Trzop <arturtrzop@gmail.com>
2024-05-16 21:35:10 +0200

    refactor: remove global var and english polishing

--- spec/knapsack_pro/hooks/queue_spec.rb
+++ spec/knapsack_pro/hooks/queue_spec.rb
@@ -2,34 +2,32 @@ describe KnapsackPro::Hooks::Queue do
   describe '.call_before_queue' do
     subject { described_class.call_before_queue }
 
-    context 'when callback is not set' do
-      before do
-        described_class.reset_before_queue
-      end
+    before do
+      described_class.reset_before_queue
+    end
 
+    context 'when no callback is set' do
       it { should be_nil }
     end
 
-    context 'when callback is set multiple times' do
+    context 'when multiple callbacks are set' do
       let(:queue_id) { double }
 
-      before do
+      it 'calls each block' do
         expect(KnapsackPro::Config::Env).to receive(:queue_id).twice.and_return(queue_id)
 
-        $expected_called_blocks = []
+        expected_called_blocks = []
 
         described_class.before_queue do |q_id|
-          $expected_called_blocks << [:block_1_called, q_id]
+          expected_called_blocks << [:block_1_called, q_id]
         end
         described_class.before_queue do |q_id|
-          $expected_called_blocks << [:block_2_called, q_id]
+          expected_called_blocks << [:block_2_called, q_id]
         end
-      end
 
-      it 'each block is called' do
         subject
 
-        expect($expected_called_blocks).to eq([
+        expect(expected_called_blocks).to eq([
           [:block_1_called, queue_id],
           [:block_2_called, queue_id],
         ])
@@ -166,34 +164,32 @@ describe KnapsackPro::Hooks::Queue do
   describe '.call_after_queue' do
     subject { described_class.call_after_queue }
 
-    context 'when callback is not set' do
-      before do
-        described_class.reset_after_queue
-      end
+    before do
+      described_class.reset_after_queue
+    end
 
+    context 'when no callback is set' do
       it { should be_nil }
     end
 
-    context 'when callback is set multiple times' do
+    context 'when multiple callbacks are set' do
       let(:queue_id) { double }
 
-      before do
+      it 'calls each block' do
         expect(KnapsackPro::Config::Env).to receive(:queue_id).twice.and_return(queue_id)
 
-        $expected_called_blocks = []
+        expected_called_blocks = []
 
         described_class.after_queue do |q_id|
-          $expected_called_blocks << [:block_1_called, q_id]
+          expected_called_blocks << [:block_1_called, q_id]
         end
         described_class.after_queue do |q_id|
-          $expected_called_blocks << [:block_2_called, q_id]
+          expected_called_blocks << [:block_2_called, q_id]
         end
-      end
 
-      it 'each block is called' do
         subject
 
-        expect($expected_called_blocks).to eq([
+        expect(expected_called_blocks).to eq([
           [:block_1_called, queue_id],
           [:block_2_called, queue_id],
         ])
