6207b0d103f29fded027d52dfea9039a4b035d60
Artur Trzop <arturtrzop@gmail.com>
2024-10-30 12:42:58 +0100

    Update rspec_merger_spec.rb

--- spec/knapsack_pro/test_case_mergers/rspec_merger_spec.rb
+++ spec/knapsack_pro/test_case_mergers/rspec_merger_spec.rb
@@ -37,22 +37,44 @@ describe KnapsackPro::TestCaseMergers::RSpecMerger do
     end
 
     context 'when test files have test example paths and the full test file path exists simultaneously' do
-      let(:test_files) do
-        [
-          { 'path' => 'spec/a_spec.rb', 'time_execution' => 1.1 },
-          # full test file path exists alongside test example paths
-          { 'path' => 'spec/test_case_spec.rb', 'time_execution' => 1.0 },
-          # test example paths
-          { 'path' => 'spec/test_case_spec.rb[1:1]', 'time_execution' => 2.2 },
-          { 'path' => 'spec/test_case_spec.rb[1:2]', 'time_execution' => 0.8 },
-        ]
+      context 'when the full test file path has a higher execution time than the sum of test example paths' do
+        let(:test_files) do
+          [
+            { 'path' => 'spec/a_spec.rb', 'time_execution' => 1.1 },
+            # full test file path exists alongside test example paths
+            { 'path' => 'spec/test_case_spec.rb', 'time_execution' => 3.1 },
+            # test example paths
+            { 'path' => 'spec/test_case_spec.rb[1:1]', 'time_execution' => 2.2 },
+            { 'path' => 'spec/test_case_spec.rb[1:2]', 'time_execution' => 0.8 },
+          ]
+        end
+
+        it 'returns the full test file path execution time' do
+          expect(subject).to eq([
+            { 'path' => 'spec/a_spec.rb', 'time_execution' => 1.1 },
+            { 'path' => 'spec/test_case_spec.rb', 'time_execution' => 3.1 },
+          ])
+        end
       end
 
-      it 'merges the paths and sums their execution times' do
-        expect(subject).to eq([
-          { 'path' => 'spec/a_spec.rb', 'time_execution' => 1.1 },
-          { 'path' => 'spec/test_case_spec.rb', 'time_execution' => 4.0 },
-        ])
+      context 'when the full test file path has a lower execution time than the sum of test example paths' do
+        let(:test_files) do
+          [
+            { 'path' => 'spec/a_spec.rb', 'time_execution' => 1.1 },
+            # full test file path exists alongside test example paths
+            { 'path' => 'spec/test_case_spec.rb', 'time_execution' => 2.9 },
+            # test example paths
+            { 'path' => 'spec/test_case_spec.rb[1:1]', 'time_execution' => 2.2 },
+            { 'path' => 'spec/test_case_spec.rb[1:2]', 'time_execution' => 0.8 },
+          ]
+        end
+
+        it "returns the sum of test example paths' execution times" do
+          expect(subject).to eq([
+            { 'path' => 'spec/a_spec.rb', 'time_execution' => 1.1 },
+            { 'path' => 'spec/test_case_spec.rb', 'time_execution' => 3.0 },
+          ])
+        end
       end
     end
   end
