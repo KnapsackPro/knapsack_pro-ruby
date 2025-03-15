c098438505c696c1162a371ce018fc79392ceb97
Artur Trzop <arturtrzop@gmail.com>
2025-03-15 20:49:34 +0100

    pseudo code: pull tests from queue

--- lib/knapsack_pro/queue_allocator.rb
+++ lib/knapsack_pro/queue_allocator.rb
@@ -12,6 +12,68 @@ module KnapsackPro
       @repository_adapter = args.fetch(:repository_adapter)
     end
 
+    def handle_fallback_mode
+      if !KnapsackPro::Config::Env.fallback_mode_enabled?
+        message = "Fallback Mode was disabled with KNAPSACK_PRO_FALLBACK_MODE_ENABLED=false. Please restart this CI node to retry tests. Most likely Fallback Mode was disabled due to #{KnapsackPro::Urls::QUEUE_MODE__CONNECTION_ERROR_WITH_FALLBACK_ENABLED_FALSE}"
+        KnapsackPro.logger.error(message)
+        raise FallbackModeError.new(message)
+      elsif KnapsackPro::Config::Env.ci_node_retry_count > 0
+        message = "knapsack_pro gem could not connect to Knapsack Pro API and the Fallback Mode cannot be used this time. Running tests in Fallback Mode are not allowed for retried parallel CI node to avoid running the wrong set of tests. Please manually retry this parallel job on your CI server then knapsack_pro gem will try to connect to Knapsack Pro API again and will run a correct set of tests for this CI node. Learn more #{KnapsackPro::Urls::QUEUE_MODE__CONNECTION_ERROR_WITH_FALLBACK_ENABLED_TRUE_AND_POSITIVE_RETRY_COUNT}"
+        unless KnapsackPro::Config::Env.fixed_queue_split?
+          message += " Please ensure you have set KNAPSACK_PRO_FIXED_QUEUE_SPLIT=true to allow Knapsack Pro API remember the recorded CI node tests so when you retry failed tests on the CI node then the same set of tests will be executed. See more #{KnapsackPro::Urls::FIXED_QUEUE_SPLIT}"
+        end
+        KnapsackPro.logger.error(message)
+        raise FallbackModeError.new(message)
+      else
+        @fallback_activated = true
+        KnapsackPro.logger.warn("Fallback mode started. We could not connect with Knapsack Pro API. Your tests will be executed based on directory names. If other CI nodes were able to connect with Knapsack Pro API then you may notice that some of the test files will be executed twice across CI nodes. The most important thing is to guarantee each of test files is run at least once! Read more about fallback mode at #{KnapsackPro::Urls::FALLBACK_MODE}")
+        fallback_test_files(executed_test_files)
+      end
+    end
+
+    def attempt_to_initialize_queue(tests)
+      # make attempt to initalize a new queue on API side
+      action = build_action(can_initialize_queue, attempt_connect_to_queue: false, tests)
+      connection = KnapsackPro::Client::Connection.new(action)
+      response = connection.call
+      if connection.success?
+        prepare_test_files(response)
+      else
+        handle_fallback_mode
+      end
+    end
+
+    def new_test_file_paths(can_initialize_queue, executed_test_files)
+      return [] if @fallback_activated
+
+      result = attempt_to_pull_tests_from_queue
+      if result.batch_fetched?
+        response = result.response
+        prepare_test_files(response)
+      elsif result.queue_not_initialized?
+        test_suite_result = test_suite_builder.fast_and_slow_test_files_to_run
+        if test_suite_result.locally_determined?
+          attempt_to_initialize_queue(test_suite_result.tests)
+        else
+          # not locally determined tests could be slow (dry run for split by test examples etc)
+          # so let's attempt to pull from queue first because it could already be initialized by other node
+          result = attempt_to_pull_tests_from_queue
+          if result.batch_fetched?
+            response = result.response
+            prepare_test_files(response)
+          elsif result.queue_not_initialized?
+            attempt_to_initialize_queue(test_suite_result.tests)
+          elsif result.failed_request?
+            handle_fallback_mode
+          end
+        end
+      elsif result.failed_request?
+        handle_fallback_mode
+      else
+        raise 'should never happen'
+      end
+    end
+
     def test_file_paths(can_initialize_queue, executed_test_files)
       return [] if @fallback_activated
       action = build_action(can_initialize_queue, attempt_connect_to_queue: can_initialize_queue)
