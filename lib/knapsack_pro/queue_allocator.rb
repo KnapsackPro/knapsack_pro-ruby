f199815dfd1581d90acb2c3efc3a0e748cb7226c
Artur Trzop <arturtrzop@gmail.com>
2025-03-19 23:01:43 +0100

    wip

--- lib/knapsack_pro/queue_allocator.rb
+++ lib/knapsack_pro/queue_allocator.rb
@@ -2,6 +2,7 @@
 
 module KnapsackPro
   class QueueAllocator
+    QueueResult = Struct.new(:batch_fetched?, :queue_exists?, :failed_connection?, :response)
     FallbackModeError = Class.new(StandardError)
 
     def initialize(args)
@@ -43,9 +44,20 @@ module KnapsackPro
       end
     end
 
-    def new_test_file_paths(can_initialize_queue, executed_test_files)
+    def test_file_paths2(can_initialize_queue, executed_test_files)
       return [] if @fallback_activated
 
+      result = attempt_to_fetch_tests_from_queue(can_initialize_queue)
+
+      if result.failed_connection?
+        switch_to_fallback_mode(executed_test_files)
+      elsif result.batch_fetched?
+        prepare_test_files(result.response)
+      else #if !result.queue_exists?
+        attempt_to_initialize_queue(test_suite_builder.fast_and_slow_test_files_to_run.tests)
+      end
+
+      # pseudo code
       result = attempt_to_pull_tests_from_queue
       if result.batch_fetched?
         response = result.response
@@ -152,6 +164,61 @@ module KnapsackPro
       test_flat_distributor = KnapsackPro::TestFlatDistributor.new(test_suite_builder.fallback_mode_test_files, ci_node_total)
       test_files_for_node_index = test_flat_distributor.test_files_for_node(ci_node_index)
       KnapsackPro::TestFilePresenter.paths(test_files_for_node_index) - executed_test_files
+    end
+
+    def attempt_to_fetch_tests_from_queue(can_initialize_queue)
+      action = build_action(can_initialize_queue, attempt_connect_to_queue: can_initialize_queue)
+      connection = KnapsackPro::Client::Connection.new(action)
+      response = connection.call
+
+      unless connection.success?
+        return QueueResult.new(
+          batch_fetched?: false,
+          queue_exists?: nil,
+          failed_connection?: true,
+          response: response
+        )
+      end
+
+      if can_initialize_queue && connection.api_code == KnapsackPro::Client::API::V1::Queues::CODE_ATTEMPT_CONNECT_TO_QUEUE_FAILED
+        return QueueResult.new(
+          batch_fetched?: false,
+          queue_exists?: false,
+          failed_connection?: false,
+          response: response
+        )
+      end
+
+      raise ArgumentError.new(response) if connection.errors?
+
+      QueueResult.new(
+        batch_fetched?: true,
+        queue_exists?: true,
+        failed_connection?: false,
+        response: response
+      )
+    end
+
+    def attempt_to_initialize_queue(tests_to_run)
+    end
+
+    def switch_to_fallback_mode(executed_test_files)
+      if !KnapsackPro::Config::Env.fallback_mode_enabled?
+        message = "Fallback Mode was disabled with KNAPSACK_PRO_FALLBACK_MODE_ENABLED=false. Please restart this CI node to retry tests. Most likely Fallback Mode was disabled due to #{KnapsackPro::Urls::QUEUE_MODE__CONNECTION_ERROR_WITH_FALLBACK_ENABLED_FALSE}"
+        KnapsackPro.logger.error(message)
+        raise FallbackModeError.new(message)
+      elsif KnapsackPro::Config::Env.ci_node_retry_count > 0
+        message = "knapsack_pro gem could not connect to Knapsack Pro API and the Fallback Mode cannot be used this time. Running tests in Fallback Mode are not allowed for retried parallel CI node to avoid running the wrong set of tests. Please manually retry this parallel job on your CI server then knapsack_pro gem will try to connect to Knapsack Pro API again and will run a correct set of tests for this CI node. Learn more #{KnapsackPro::Urls::QUEUE_MODE__CONNECTION_ERROR_WITH_FALLBACK_ENABLED_TRUE_AND_POSITIVE_RETRY_COUNT}"
+        unless KnapsackPro::Config::Env.fixed_queue_split?
+          message += " Please ensure you have set KNAPSACK_PRO_FIXED_QUEUE_SPLIT=true to allow Knapsack Pro API remember the recorded CI node tests so when you retry failed tests on the CI node then the same set of tests will be executed. See more #{KnapsackPro::Urls::FIXED_QUEUE_SPLIT}"
+        end
+        KnapsackPro.logger.error(message)
+        raise FallbackModeError.new(message)
+      else
+        @fallback_activated = true
+        KnapsackPro.logger.warn("Fallback mode started. We could not connect with Knapsack Pro API. Your tests will be executed based on directory names. If other CI nodes were able to connect with Knapsack Pro API then you may notice that some of the test files will be executed twice across CI nodes. The most important thing is to guarantee each of test files is run at least once! Read more about fallback mode at #{KnapsackPro::Urls::FALLBACK_MODE}")
+        fallback_test_files(executed_test_files)
+      end
     end
   end
 end
