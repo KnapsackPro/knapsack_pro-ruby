fa6eb569bd0ad09c56a95427492bb6956f9196bf
John Jones <john.jones@bugcrowd.com>
2025-05-06 13:34:46 -0700

    I think that is all of them

--- lib/knapsack_pro/runners/spinach_runner.rb
+++ lib/knapsack_pro/runners/spinach_runner.rb
@@ -15,9 +15,11 @@ module KnapsackPro
 
           KnapsackPro.tracker.set_prerun_tests(runner.test_file_paths)
 
-          cmd = %Q[KNAPSACK_PRO_REGULAR_MODE_ENABLED=true KNAPSACK_PRO_TEST_SUITE_TOKEN=#{ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN']} bundle exec spinach #{args} --features_path #{runner.test_dir} -- #{runner.stringify_test_file_paths}]
-
-          Kernel.system(cmd)
+          cmd = %Q[bundle exec spinach #{args} --features_path #{runner.test_dir} -- #{runner.stringify_test_file_paths}]
+          Kernel.system({
+            'KNAPSACK_PRO_REGULAR_MODE_ENABLED' => 'true',
+            'KNAPSACK_PRO_TEST_SUITE_TOKEN' => ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN']
+          }, cmd)
           Kernel.exit(child_status.exitstatus) unless child_status.exitstatus.zero?
         end
       end
--- lib/tasks/queue/minitest.rake
+++ lib/tasks/queue/minitest.rake
@@ -5,7 +5,10 @@ require 'knapsack_pro'
 namespace :knapsack_pro do
   namespace :queue do
     task :minitest, [:minitest_args] do |_, args|
-      Kernel.exec("RAILS_ENV=test RACK_ENV=test #{$PROGRAM_NAME} 'knapsack_pro:queue:minitest_go[#{args[:minitest_args]}]'")
+      Kernel.exec(
+        { 'RAILS_ENV' => 'test', 'RACK_ENV' => 'test' },
+        "#{$PROGRAM_NAME} 'knapsack_pro:queue:minitest_go[#{args[:minitest_args]}]'"
+      )
     end
 
     task :minitest_go, [:minitest_args] do |_, args|
