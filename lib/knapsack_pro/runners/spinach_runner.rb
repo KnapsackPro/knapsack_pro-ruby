6cd3825c2a0bed3f3d284ef616878ec7936032fa
Artur Trzop <arturtrzop@gmail.com>
2025-05-15 22:43:07 +0200

    use exec for spinach in regular mode

--- lib/knapsack_pro/runners/spinach_runner.rb
+++ lib/knapsack_pro/runners/spinach_runner.rb
@@ -26,11 +26,10 @@ module KnapsackPro
             runner.stringify_test_file_paths
           ]
 
-          Kernel.system({
+          Kernel.exec({
             'KNAPSACK_PRO_REGULAR_MODE_ENABLED' => 'true',
             'KNAPSACK_PRO_TEST_SUITE_TOKEN' => ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN']
           }, cmd)
-          Kernel.exit(child_status.exitstatus) unless child_status.exitstatus.zero?
         end
       end
     end
