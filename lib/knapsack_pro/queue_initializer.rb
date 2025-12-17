890d99382e3dbc68de49d7bc86a7bc07cdf35faf
3v0k4 <riccardo.odone@gmail.com>
2025-12-17 16:12:43 +0100

    tweaks

--- lib/knapsack_pro/queue_initializer.rb
+++ lib/knapsack_pro/queue_initializer.rb
@@ -5,7 +5,7 @@ module KnapsackPro
     class QueueInitializer
       def call(args)
         slow_id_paths = KnapsackPro::TestCaseDetectors::RSpecTestExampleDetector.new.calculate_slow_id_paths(args.to_s)
-        all_test_files_to_run = KnapsackPro::TestSuite.new(KnapsackPro::Adapters::RspecAdapter).all_test_files_to_run
+        all_test_files_to_run = KnapsackPro::TestSuite.new(KnapsackPro::Adapters::RSpecAdapter).all_test_files_to_run
         paths = KnapsackPro::Adapters::RSpecAdapter.concat_paths(all_test_files_to_run, slow_id_paths)
 
         raise 'No paths to run' if paths.empty?
