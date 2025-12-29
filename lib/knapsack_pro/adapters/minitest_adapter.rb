3354b8310bd0ad63411012f7cad2c4f2424e357b
Riccardo <riccardo.odone@gmail.com>
2025-12-29 09:31:02 +0100

    Minitest v6 (#321)

--- lib/knapsack_pro/adapters/minitest_adapter.rb
+++ lib/knapsack_pro/adapters/minitest_adapter.rb
@@ -7,19 +7,14 @@ module KnapsackPro
       @@parent_of_test_dir = nil
 
       def self.test_path(obj)
-        # Pick the first public method in the class itself, that starts with "test_"
-        test_method_name = obj.public_methods(false).select{|m| m =~ /^test_/ }.first
-        if test_method_name.nil?
-          # case for shared examples
-          method_object = obj.method(obj.location.sub(/.*?test_/, 'test_'))
-        else
-          method_object = obj.method(test_method_name)
-        end
-        full_test_path = method_object.source_location.first
-        parent_of_test_dir_regexp = Regexp.new("^#{@@parent_of_test_dir}")
-        test_path = full_test_path.gsub(parent_of_test_dir_regexp, '.')
-        # test_path will look like ./test/dir/unit_test.rb
-        test_path
+        path, _line =
+          begin
+            Object.const_source_location(obj.class.to_s)
+          rescue NameError
+            test_method_name = obj.class.runnable_methods.first
+            obj.method(test_method_name).source_location
+          end
+        path.gsub(Regexp.new("^#{@@parent_of_test_dir}"), '.')
       end
 
       # See how to write hooks and plugins
