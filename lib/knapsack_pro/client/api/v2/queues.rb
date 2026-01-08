01166424b0616d93b08b4a99512c5e2957de12cc
3v0k4 <riccardo.odone@gmail.com>
2026-01-08 11:16:12 +0100

    feat(rof): rake task to initialize queue

--- lib/knapsack_pro/client/api/v2/queues.rb
+++ lib/knapsack_pro/client/api/v2/queues.rb
@@ -10,24 +10,23 @@ module KnapsackPro
           class << self
             def queue(args)
               request_hash = {
-                fixed_queue_split: KnapsackPro::Config::Env.fixed_queue_split,
-                can_initialize_queue: args.fetch(:can_initialize_queue),
                 attempt_connect_to_queue: args.fetch(:attempt_connect_to_queue),
-                commit_hash: args.fetch(:commit_hash),
+                batch_uuid: args.fetch(:batch_uuid),
                 branch: args.fetch(:branch),
-                node_total: args.fetch(:node_total),
+                can_initialize_queue: args.fetch(:can_initialize_queue),
+                commit_hash: args.fetch(:commit_hash),
                 node_index: args.fetch(:node_index),
-                user_seat: KnapsackPro::Config::Env.masked_user_seat,
-                test_queue_id: KnapsackPro::Config::Env.test_queue_id,
+                node_total: args.fetch(:node_total),
                 node_uuid: KnapsackPro::Config::Env.node_uuid,
-                batch_uuid: args.fetch(:batch_uuid)
+                test_queue_id: KnapsackPro::Config::Env.test_queue_id,
+                user_seat: KnapsackPro::Config::Env.masked_user_seat
               }
 
               if request_hash[:can_initialize_queue] && !request_hash[:attempt_connect_to_queue]
                 request_hash.merge!(
-                  test_files: args.fetch(:test_files),
                   build_author: KnapsackPro::RepositoryAdapters::GitAdapter.new.build_author,
-                  commit_authors: KnapsackPro::RepositoryAdapters::GitAdapter.new.commit_authors
+                  commit_authors: KnapsackPro::RepositoryAdapters::GitAdapter.new.commit_authors,
+                  test_files: args.fetch(:test_files)
                 )
               end
 
@@ -36,6 +35,34 @@ module KnapsackPro
                   failed_paths: args.fetch(:failed_paths)
                 )
               end
+
+              action_class.new(
+                endpoint_path: '/v2/queues/queue',
+                http_method: :post,
+                request_hash: request_hash
+              )
+            end
+
+            def initialize(paths)
+              git_adapter = KnapsackPro::RepositoryAdapters::GitAdapter.new
+              repository_adapter = KnapsackPro::RepositoryAdapterInitiator.call
+
+              request_hash = {
+                attempt_connect_to_queue: false,
+                batch_uuid: SecureRandom.uuid,
+                branch: KnapsackPro::Crypto::BranchEncryptor.call(repository_adapter.branch),
+                build_author: git_adapter.build_author,
+                can_initialize_queue: true,
+                commit_authors: git_adapter.commit_authors,
+                commit_hash: repository_adapter.commit_hash,
+                node_index: KnapsackPro::Config::Env.ci_node_index,
+                node_total: KnapsackPro::Config::Env.ci_node_total,
+                node_uuid: SecureRandom.uuid,
+                skip_pull: true,
+                test_files: KnapsackPro::Crypto::Encryptor.call(paths),
+                test_queue_id: KnapsackPro::Config::Env.test_queue_id,
+                user_seat: KnapsackPro::Config::Env.masked_user_seat,
+              }
 
               action_class.new(
                 endpoint_path: '/v2/queues/queue',
