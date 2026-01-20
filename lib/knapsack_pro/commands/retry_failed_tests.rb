a846970d2e1c724893f64d1a9605dde2d9e8b0da
3v0k4 <riccardo.odone@gmail.com>
2026-01-20 16:11:11 +0100

    feat: tweak retry command

---
+++ lib/knapsack_pro/commands/retry_failed_tests.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "json"
+require "net/http"
+
+class RetryFailedTests
+  def initialize(branch:)
+    @branch = branch || `git branch --show-current`.chomp
+  end
+
+  def call(runner_args)
+    $stderr.puts "Branch: #{branch}"
+
+    failed_paths = fetch_failed_paths
+    return ($stderr.puts "Nothing to run") if failed_paths.size.zero?
+
+    $stderr.puts "Retrying #{failed_paths.size} tests..."
+    exec Gem.bin_path("rspec-core", "rspec"), *(runner_args + failed_paths)
+  end
+
+  private
+
+  attr_accessor :branch
+
+  def fetch_failed_paths
+    headers = {
+      'Accept' => 'application/json',
+      'KNAPSACK-PRO-TEST-SUITE-TOKEN' => ENV.fetch("KNAPSACK_PRO_TEST_SUITE_TOKEN")
+    }
+
+    uri = URI.parse("#{KnapsackPro::Config::Env.endpoint}/v2/test_paths")
+    uri.query = URI.encode_www_form(branch: branch)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+    http.open_timeout = 5
+    http.read_timeout = 5
+
+    response = http.get(uri.request_uri, headers)
+    abort response.inspect if (300..).cover?(response.code.to_i)
+
+    parsed = JSON.parse(response.body)
+    abort parsed.inspect if parsed['errors']
+
+    parsed.fetch('failed_paths')
+  end
+end
