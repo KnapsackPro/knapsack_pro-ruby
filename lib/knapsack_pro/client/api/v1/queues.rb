e93179979bf82b9a9a71dc5b91bc796796a78319
3v0k4 <riccardo.odone@gmail.com>
2026-01-20 16:11:21 +0100

    chore: remove unused code

--- lib/knapsack_pro/client/api/v1/queues.rb
+++ lib/knapsack_pro/client/api/v1/queues.rb
@@ -36,59 +36,6 @@ module KnapsackPro
                 request_hash: request_hash
               )
             end
-
-            def initialize(paths)
-              git_adapter = KnapsackPro::RepositoryAdapters::GitAdapter.new
-              repository_adapter = KnapsackPro::RepositoryAdapterInitiator.call
-
-              request_hash = {
-                attempt_connect_to_queue: false,
-                branch: KnapsackPro::Crypto::BranchEncryptor.call(repository_adapter.branch),
-                build_author: git_adapter.build_author,
-                can_initialize_queue: true,
-                commit_authors: git_adapter.commit_authors,
-                commit_hash: repository_adapter.commit_hash,
-                fixed_queue_split: KnapsackPro::Config::Env.fixed_queue_split,
-                node_build_id: KnapsackPro::Config::Env.ci_node_build_id,
-                node_index: KnapsackPro::Config::Env.ci_node_index,
-                node_total: KnapsackPro::Config::Env.ci_node_total,
-                skip_pull: true,
-                test_files: KnapsackPro::Crypto::Encryptor.call(paths),
-                user_seat: KnapsackPro::Config::Env.masked_user_seat,
-              }
-
-              action_class.new(
-                endpoint_path: '/v1/queues/queue',
-                http_method: :post,
-                request_hash: request_hash
-              )
-            end
-
-            def connect
-              git_adapter = KnapsackPro::RepositoryAdapters::GitAdapter.new
-              repository_adapter = KnapsackPro::RepositoryAdapterInitiator.call
-
-              request_hash = {
-                attempt_connect_to_queue: true,
-                branch: KnapsackPro::Crypto::BranchEncryptor.call(repository_adapter.branch),
-                build_author: git_adapter.build_author,
-                can_initialize_queue: true,
-                commit_authors: git_adapter.commit_authors,
-                commit_hash: repository_adapter.commit_hash,
-                fixed_queue_split: KnapsackPro::Config::Env.fixed_queue_split,
-                node_build_id: KnapsackPro::Config::Env.ci_node_build_id,
-                node_index: KnapsackPro::Config::Env.ci_node_index,
-                node_total: KnapsackPro::Config::Env.ci_node_total,
-                skip_pull: true,
-                user_seat: KnapsackPro::Config::Env.masked_user_seat,
-              }
-
-              action_class.new(
-                endpoint_path: '/v1/queues/queue',
-                http_method: :post,
-                request_hash: request_hash
-              )
-            end
           end
         end
       end
