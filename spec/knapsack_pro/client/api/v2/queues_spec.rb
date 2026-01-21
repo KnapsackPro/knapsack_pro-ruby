066f2ffffa36ed97ee44d10fb1e38f484ddc0e83
3v0k4 <riccardo.odone@gmail.com>
2026-01-21 11:14:43 +0100

    feat: send fixed queue split in v2

--- spec/knapsack_pro/client/api/v2/queues_spec.rb
+++ spec/knapsack_pro/client/api/v2/queues_spec.rb
@@ -1,18 +1,19 @@
 describe KnapsackPro::Client::API::V2::Queues do
   describe '.queue' do
-    let(:commit_hash) { double }
-    let(:branch) { double }
-    let(:node_total) { double }
-    let(:node_index) { double }
-    let(:test_files) { double }
-    let(:masked_user_seat) { double }
-    let(:can_initialize_queue) { [false, true].sample }
     let(:attempt_connect_to_queue) { [false, true].sample }
-    let(:test_queue_id) { "123:abc" }
-    let(:node_uuid) { SecureRandom.uuid }
     let(:batch_uuid) { SecureRandom.uuid }
+    let(:branch) { double }
+    let(:can_initialize_queue) { [false, true].sample }
+    let(:commit_hash) { double }
+    let(:fixed_queue_split_?) { [false, true].sample }
+    let(:masked_user_seat) { double }
+    let(:node_index) { double }
+    let(:node_total) { double }
+    let(:node_uuid) { SecureRandom.uuid }
+    let(:test_queue_id) { "123:abc" }
 
     before(:each) do
+      expect(KnapsackPro::Config::Env).to receive(:fixed_queue_split_?).and_return(fixed_queue_split_?)
       expect(KnapsackPro::Config::Env).to receive(:masked_user_seat).and_return(masked_user_seat)
       expect(KnapsackPro::Config::Env).to receive(:node_uuid).and_return(node_uuid)
       expect(KnapsackPro::Config::Env).to receive(:test_queue_id).and_return(test_queue_id)
@@ -23,122 +24,87 @@ describe KnapsackPro::Client::API::V2::Queues do
       let(:attempt_connect_to_queue) { true }
 
       it do
-        expected = KnapsackPro::Client::API::Action.new(
-          endpoint_path: '/v2/queues/queue',
-          http_method: :post,
-          request_hash: {
-            can_initialize_queue: true,
-            attempt_connect_to_queue: true,
-            commit_hash: commit_hash,
-            branch: branch,
-            node_total: node_total,
-            node_index: node_index,
-            user_seat: masked_user_seat,
-            test_queue_id: test_queue_id,
-            node_uuid: node_uuid,
-            batch_uuid: batch_uuid
-          }
-        )
-
         actual = described_class.queue(
-          can_initialize_queue: can_initialize_queue,
           attempt_connect_to_queue: attempt_connect_to_queue,
-          commit_hash: commit_hash,
+          batch_uuid: batch_uuid,
           branch: branch,
-          node_total: node_total,
+          can_initialize_queue: can_initialize_queue,
+          commit_hash: commit_hash,
           node_index: node_index,
-          test_files: test_files,
-          batch_uuid: batch_uuid
+          node_total: node_total
         )
 
-        expect(actual).to eq(expected)
+        expected_request_hash = {
+          attempt_connect_to_queue: attempt_connect_to_queue,
+          batch_uuid: batch_uuid,
+          branch: branch,
+          can_initialize_queue: can_initialize_queue,
+          commit_hash: commit_hash,
+          fixed_queue_split: fixed_queue_split_?,
+          node_index: node_index,
+          node_total: node_total,
+          node_uuid: node_uuid,
+          test_queue_id: test_queue_id,
+          user_seat: masked_user_seat
+        }
+        expect(actual.endpoint_path).to eq("/v2/queues/queue")
+        expect(actual.http_method).to eq(:post)
+        expect(actual.request_hash).to include(expected_request_hash)
       end
     end
 
     context 'when can_initialize_queue=true and attempt_connect_to_queue=false' do
-      let(:can_initialize_queue) { true }
       let(:attempt_connect_to_queue) { false }
       let(:build_author) { '3v0*4 <ri******.od***@gm***.co*>' }
+      let(:can_initialize_queue) { true }
       let(:commit_authors) { [{ commits: 2, author: build_author }] }
+      let(:test_files) { ["spec/a_spec.rb"] }
 
       before(:each) do
         allow_any_instance_of(KnapsackPro::RepositoryAdapters::GitAdapter).to receive(:build_author).and_return(build_author)
         allow_any_instance_of(KnapsackPro::RepositoryAdapters::GitAdapter).to receive(:commit_authors).and_return(commit_authors)
       end
 
-      it do
-        expected = KnapsackPro::Client::API::Action.new(
-          endpoint_path: '/v2/queues/queue',
-          http_method: :post,
-          request_hash: {
-            can_initialize_queue: true,
-            attempt_connect_to_queue: false,
-            commit_hash: commit_hash,
-            branch: branch,
-            node_total: node_total,
-            node_index: node_index,
-            user_seat: masked_user_seat,
-            test_files: test_files,
-            build_author: build_author,
-            commit_authors: commit_authors,
-            test_queue_id: test_queue_id,
-            node_uuid: node_uuid,
-            batch_uuid: batch_uuid
-          }
-        )
-
+      it "includes build_authors, commit_authors, test_files" do
         actual = described_class.queue(
-          can_initialize_queue: can_initialize_queue,
           attempt_connect_to_queue: attempt_connect_to_queue,
-          commit_hash: commit_hash,
+          batch_uuid: batch_uuid,
           branch: branch,
-          node_total: node_total,
+          can_initialize_queue: can_initialize_queue,
+          commit_hash: commit_hash,
           node_index: node_index,
-          test_files: test_files,
-          batch_uuid: batch_uuid
+          node_total: node_total,
+          test_files: test_files
         )
 
-        expect(actual).to eq(expected)
+        expect(actual.request_hash).to include(
+          build_author: build_author,
+          commit_authors: commit_authors,
+          test_files: test_files
+        )
       end
     end
 
     context 'when can_initialize_queue=false and attempt_connect_to_queue=false' do
-      let(:can_initialize_queue) { false }
       let(:attempt_connect_to_queue) { false }
+      let(:can_initialize_queue) { false }
       let(:failed_paths) { ['spec/a_spec.rb[1:1]'] }
+      let(:test_files) { ["spec/a_spec.rb"] }
 
-      it do
-        expected = KnapsackPro::Client::API::Action.new(
-          endpoint_path: '/v2/queues/queue',
-          http_method: :post,
-          request_hash: {
-            can_initialize_queue: false,
-            attempt_connect_to_queue: false,
-            commit_hash: commit_hash,
-            branch: branch,
-            node_total: node_total,
-            node_index: node_index,
-            user_seat: masked_user_seat,
-            test_queue_id: test_queue_id,
-            node_uuid: node_uuid,
-            failed_paths: failed_paths,
-            batch_uuid: batch_uuid
-          }
-        )
-
+      it "includes failed_paths" do
         actual = described_class.queue(
-          can_initialize_queue: can_initialize_queue,
           attempt_connect_to_queue: attempt_connect_to_queue,
-          commit_hash: commit_hash,
+          batch_uuid: batch_uuid,
           branch: branch,
-          node_total: node_total,
-          node_index: node_index,
-          test_files: test_files,
+          can_initialize_queue: can_initialize_queue,
+          commit_hash: commit_hash,
           failed_paths: failed_paths,
-          batch_uuid: batch_uuid
+          node_index: node_index,
+          node_total: node_total,
+          test_files: test_files
         )
 
-        expect(actual).to eq(expected)
+        expect(actual.request_hash).to include(failed_paths: failed_paths)
       end
     end
   end
