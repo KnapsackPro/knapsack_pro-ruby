ac79e58ff86b3f507d07f84e7915f33d3d01832c
3v0k4 <riccardo.odone@gmail.com>
2026-02-05 16:22:46 +0100

    test: regenerate rails-app

--- rails-app-with-knapsack_pro/db/schema.rb
+++ rails-app-with-knapsack_pro/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_22_175834) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_05_152056) do
   create_table "articles", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
