8dcfc3ac0939ec393d18d53fedae3aeca7ad385f
3v0k4 <riccardo.odone@gmail.com>
2026-02-09 19:27:03 +0100

    feat: rake init via v2

--- lib/knapsack_pro/client/api/v2/queues.rb
+++ lib/knapsack_pro/client/api/v2/queues.rb
@@ -46,6 +46,34 @@ module KnapsackPro
               )
             end
 
+            def connect
+              git_adapter = KnapsackPro::RepositoryAdapters::GitAdapter.new
+              repository_adapter = KnapsackPro::RepositoryAdapterInitiator.call
+
+              request_hash = {
+                attempt_connect_to_queue: true,
+                batch_uuid: SecureRandom.uuid,
+                branch: KnapsackPro::Crypto::BranchEncryptor.call(repository_adapter.branch),
+                build_author: git_adapter.build_author,
+                can_initialize_queue: true,
+                commit_authors: git_adapter.commit_authors,
+                commit_hash: repository_adapter.commit_hash,
+                fixed_queue_split: KnapsackPro::Config::Env.fixed_queue_split_?,
+                node_index: KnapsackPro::Config::Env.ci_node_index,
+                node_total: KnapsackPro::Config::Env.ci_node_total,
+                node_uuid: SecureRandom.uuid,
+                skip_pull: true,
+                test_queue_id: KnapsackPro::Config::Env.test_queue_id,
+                user_seat: KnapsackPro::Config::Env.masked_user_seat,
+              }
+
+              action_class.new(
+                endpoint_path: '/v2/queues/queue',
+                http_method: :post,
+                request_hash: request_hash
+              )
+            end
+
             def initialize(paths)
               git_adapter = KnapsackPro::RepositoryAdapters::GitAdapter.new
               repository_adapter = KnapsackPro::RepositoryAdapterInitiator.call
